d703304e37c49eadafd444911bc56ef6b891937c
Daniel De Lucca <delucca@pm.me>
2020-10-27 15:25:30 -0300

    Feature: Initial setup (#1)
    * docs: adds basic documentation and templates
    
    * feature(nextjs): adds basic NextJS structure
    
    * feature: integrates with Yarn and finishes
    
    * feautre: fixes lint and added some configs

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index = () => (
+  <div>Running</div>
+)
+
+export default Index
---
+++ src/server/config.ts
@@ -0,0 +1,15 @@
+const { PORT, NODE_ENV, HOST } = process.env
+
+type ServerConfig = {
+  port: string
+  dev: boolean
+  host: string
+}
+
+const config: ServerConfig = {
+  port: PORT || '3000',
+  dev: NODE_ENV !== 'production',
+  host: HOST || 'http://local.getbud.co',
+}
+
+export default config
