dd140551b8c9a2a795b306a9d3fbf14b16656509
Daniel De Lucca <delucca@pm.me>
2021-01-12 12:06:59 -0300

    Bugfix: V0 minor bugs (#43)
    * chore(chakra): bumps version and removes old bugs
    
    * bugfix(checkInForm): fixes goal alignment
    
    Solves: BUD-288@Jira
    
    * bugfix(dashboard): fixes overlapped progress value
    
    Solves #BUD-328@Jira
    
    * bugfix(progressSlider): fixes current progress value upon new key result
    
    Solves: #BUD-285@Jira

--- src/state/recoil/key-result/selectors/current-progress.ts
+++ src/state/recoil/key-result/selectors/current-progress.ts
@@ -1,8 +1,28 @@
-import { KeyResult } from 'src/components/KeyResult/types'
-import { buildPartialSelector } from 'src/state/recoil/key-result/selectors'
+import { selectorFamily } from 'recoil'
 
-export const selectCurrentProgress = buildPartialSelector<KeyResult['currentProgress']>(
-  'currentProgress',
-)
+import { KeyResult } from 'src/components/KeyResult/types'
+import keyResultAtomFamily from 'src/state/recoil/key-result/atom-family'
+import { RecoilInterfaceGetter } from 'src/state/recoil/types'
+
+import { PREFIX } from './constants'
+
+const KEY = `${PREFIX}::SELECT_CURRENT_PROGRESS`
+
+export const getCurrentProgress = (id?: KeyResult['id']) => ({ get }: RecoilInterfaceGetter) => {
+  if (!id) return 0
+
+  const keyResult = get(keyResultAtomFamily(id))
+  if (!keyResult) return 0
+
+  return keyResult.currentProgress ?? keyResult.initialValue ?? 0
+}
+
+const selectCurrentProgress = selectorFamily<
+  KeyResult['currentProgress'],
+  KeyResult['id'] | undefined
+>({
+  key: KEY,
+  get: getCurrentProgress,
+})
 
 export default selectCurrentProgress
