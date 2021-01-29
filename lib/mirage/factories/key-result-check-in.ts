f332d1ebb3de2e3637dc0ae3db06b913beb80656
delucca <delucca+pc@pm.me>
2021-01-29 10:11:56 -0300

    chore(mirage): updats schema

--- lib/mirage/factories/key-result-check-in.ts
+++ lib/mirage/factories/key-result-check-in.ts
@@ -4,5 +4,7 @@ import { Factory } from 'miragejs'
 export default Factory.extend({
   comment: () => faker.helpers.randomize([undefined, faker.lorem.paragraph()]),
   confidence: () => faker.random.number({ min: -1, max: 100 }),
+  percentageProgressIncrease: () => faker.random.number({ min: -100, max: 100 }),
+  absoluteConfidenceIncrease: () => faker.random.number({ min: -100, max: 100 }),
   createdAt: faker.date.past,
 })
