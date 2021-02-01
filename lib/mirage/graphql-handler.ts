290a5c94a2d982248ed0db13b098536612d21707
Daniel De Lucca <delucca@pm.me>
2021-02-01 19:02:28 -0300

    Bugfix: Timeline (#66)
    * chore(mirage): fix create check-in issues
    
    * bugfix: fixes many minor bugs

--- lib/mirage/graphql-handler.ts
+++ lib/mirage/graphql-handler.ts
@@ -1,5 +1,8 @@
 import { createGraphQLHandler } from '@miragejs/graphql'
+import { pickBy } from 'lodash'
 import { ModelInstance } from 'miragejs'
+
+import { KeyResultCheckIn } from 'src/components/KeyResult/types'
 
 import Models from './models'
 import graphQLSchema from './schema.gql'
@@ -10,6 +13,10 @@ export interface QueryKeyResultReportsArguments {
 
 export interface QueryUserCompaniesArguments {
   limit?: number
+}
+
+export interface CreateKeyResultCheckInInterface {
+  keyResultCheckIn: Partial<KeyResultCheckIn>
 }
 
 const graphQLHandler = (mirageSchema: unknown) =>
@@ -31,6 +38,40 @@ const graphQLHandler = (mirageSchema: unknown) =>
           { limit }: QueryUserCompaniesArguments,
         ): Array<ModelInstance<typeof Models.keyResultCheckIn>> =>
           limit ? parent.companies.models.slice(0, limit) : parent.companies.models,
+      },
+
+      Mutation: {
+        createKeyResultCheckIn: (
+          _: any,
+          { keyResultCheckIn }: CreateKeyResultCheckInInterface,
+          { mirageSchema }: any,
+        ) => {
+          const user = mirageSchema.users.first()
+          // eslint-disable-next-line unicorn/no-fn-reference-in-iterator
+          const keyResult = mirageSchema.keyResults.find(keyResultCheckIn.keyResultId)
+          const latestKeyResultCheckInId = keyResult.attrs.keyResultCheckInIds[0]
+          const progress = keyResultCheckIn?.progress ?? 0
+
+          const relativePercentageProgress =
+            ((keyResult.goal - progress) / (keyResult.goal - keyResult.initialValue)) * 100
+          const normalizedMinRelativeProgress =
+            relativePercentageProgress < 0 ? 0 : relativePercentageProgress
+          const normalizedMaxRelativeProgress =
+            normalizedMinRelativeProgress > 100 ? 100 : normalizedMinRelativeProgress
+
+          const enhancedKeyResultCheckIn = {
+            relativePercentageProgress: normalizedMaxRelativeProgress,
+            userId: user.id,
+            parentId: latestKeyResultCheckInId,
+            createdAt: new Date(),
+            ...keyResultCheckIn,
+          }
+          const clearedKeyResultCheckIn = pickBy(enhancedKeyResultCheckIn)
+
+          const result = mirageSchema.keyResultCheckIns.create(clearedKeyResultCheckIn)
+
+          return result
+        },
       },
     },
   } as any)
