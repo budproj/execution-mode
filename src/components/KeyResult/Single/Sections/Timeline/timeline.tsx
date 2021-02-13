a49508a13b8db00bc8f907b24b22b9bf377251a3
delucca <delucca+pc@pm.me>
2021-02-13 14:34:47 -0300

    bugfix(objective): fixes objective title alignment in card
    Linked issue: #BBCR-85@Jira

--- src/components/KeyResult/Single/Sections/Timeline/timeline.tsx
+++ src/components/KeyResult/Single/Sections/Timeline/timeline.tsx
@@ -60,6 +60,7 @@ const KeyResultSectionTimeline = ({
       },
       skip: hasTimeline,
       onCompleted: handleQueryResult,
+      // The following fetchPolicy condition is required until https://github.com/apollographql/apollo-client/issues/7689 is solved
       fetchPolicy: hasTimeline ? 'cache-only' : 'network-only',
     },
   )
