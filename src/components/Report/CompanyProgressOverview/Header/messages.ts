e1de9118117286c10d6830796594682b2916ff17
delucca <delucca+pc@pm.me>
2021-02-18 19:15:54 -0300

    fix messages

--- src/components/Report/CompanyProgressOverview/Header/messages.ts
+++ src/components/Report/CompanyProgressOverview/Header/messages.ts
@@ -5,8 +5,8 @@ type CompanyProgressOverviewHeaderMessage = 'title'
 export default defineMessages<CompanyProgressOverviewHeaderMessage>({
   title: {
     defaultMessage:
-      'Como vai o desempenho d{gender, select, MALE {o} FEMALE {a} other {o}} <highlight>{company}</highlight> como um todo?',
-    id: '6KET/Z',
+      'Como vai o desempenho d{gender, select, MALE {o} FEMALE {a} other {o}} <highlight>{company}</highlight> nesse trimestre?',
+    id: 'z4STCs',
     description: 'The title of the header section in our company overview report',
   },
 })
