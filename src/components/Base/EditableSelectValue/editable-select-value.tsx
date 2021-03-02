83eeee0ea50c07a60fb8bff7d06bcaf16452dec1
delucca <delucca+pc@pm.me>
2021-03-02 15:37:46 -0300

    feature(settings): adds backend integration to social media
    Linked issues: #BBCR-216@Jira

--- src/components/Base/EditableSelectValue/editable-select-value.tsx
+++ src/components/Base/EditableSelectValue/editable-select-value.tsx
@@ -1,17 +1,28 @@
-import { Menu, MenuButton, MenuList, MenuOptionGroup, MenuProps, Text } from '@chakra-ui/react'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuOptionGroup,
+  MenuProps,
+  Text,
+  Skeleton,
+} from '@chakra-ui/react'
 import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
 
+import buildSkeletonMinSize from 'lib/chakra/build-skeleton-min-size'
 import PenIcon from 'src/components/Icon/Pen'
 
 import messages from './messages'
 
 export interface EditableSelectValueProperties extends MenuProps {
   onChange: (value: string | string[]) => void
+  skeletonWidth: number
+  skeletonHeight: number
+  isLoaded: boolean
   value?: string
   placeholder?: string
   customFallbackPlaceholder?: string
-  isLoaded?: boolean
   isSubmitting?: boolean
 }
 
@@ -23,6 +34,9 @@ const EditableSelectValue = ({
   customFallbackPlaceholder,
   children,
   isSubmitting,
+  isLoaded,
+  skeletonWidth,
+  skeletonHeight,
 }: EditableSelectValueProperties) => {
   const [isHovering, setIsHovering] = useState(false)
   const intl = useIntl()
@@ -40,36 +54,47 @@ const EditableSelectValue = ({
   }
 
   return (
-    <Menu>
-      <MenuButton
-        id={id}
-        as={Text}
-        fontSize="md"
-        fontWeight={400}
-        color={isHovering ? 'brand.500' : defaultColor}
-        py={1}
-        cursor="pointer"
-        onMouseEnter={handleStartHover}
-        onMouseLeave={handleEndHover}
-      >
-        {placeholder ?? fallbackPlaceholder}
-        <PenIcon
-          fill="brand.400"
-          opacity={isHovering ? 1 : 0}
-          display={isSubmitting ? 'none' : 'inherit'}
-          transition="opacity .2s ease-out"
-          desc={intl.formatMessage(messages.editableIconDesc)}
-          title={intl.formatMessage(messages.editableIconTitle)}
-          ml={2}
-        />
-      </MenuButton>
-      <MenuList>
-        <MenuOptionGroup value={value} type="radio" onChange={onChange}>
-          {children}
-        </MenuOptionGroup>
-      </MenuList>
-    </Menu>
+    <Skeleton
+      isLoaded={isLoaded}
+      {...buildSkeletonMinSize(isLoaded, skeletonWidth, skeletonHeight)}
+    >
+      <Menu>
+        <MenuButton
+          id={id}
+          as={Text}
+          fontSize="md"
+          fontWeight={400}
+          color={isHovering ? 'brand.500' : defaultColor}
+          py={1}
+          cursor="pointer"
+          onMouseEnter={handleStartHover}
+          onMouseLeave={handleEndHover}
+        >
+          {placeholder ?? fallbackPlaceholder}
+          <PenIcon
+            fill="brand.400"
+            opacity={isHovering ? 1 : 0}
+            display={isSubmitting ? 'none' : 'inherit'}
+            transition="opacity .2s ease-out"
+            desc={intl.formatMessage(messages.editableIconDesc)}
+            title={intl.formatMessage(messages.editableIconTitle)}
+            ml={2}
+          />
+        </MenuButton>
+        <MenuList>
+          <MenuOptionGroup value={value} type="radio" onChange={onChange}>
+            {children}
+          </MenuOptionGroup>
+        </MenuList>
+      </Menu>
+    </Skeleton>
   )
+}
+
+EditableSelectValue.defaultProps = {
+  isLoaded: true,
+  skeletonWidth: 200,
+  skeletonHeight: 19,
 }
 
 export default EditableSelectValue
