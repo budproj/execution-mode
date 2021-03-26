3bc6df6ed53d79ca88a6f54e61ce7b5ba913840b
Daniel De Lucca <delucca@pm.me>
2021-03-26 18:54:27 +0000

    fix

--- src/components/Page/MyKeyResults/Switcher/skeleton.tsx
+++ src/components/Page/MyKeyResults/Switcher/skeleton.tsx
@@ -1,11 +1,20 @@
-import { Skeleton, Stack } from '@chakra-ui/react'
+import { Skeleton, Stack, useTheme } from '@chakra-ui/react'
 import React from 'react'
 
-const MyKeyResultsPageSwitcherSkeleton = () => (
-  <Stack direction="column" spacing={4} borderBottomColor="black.100" borderBottomWidth={1}>
-    <Skeleton w={32} h={21} />
-    <Skeleton w={32} h={21} />
-  </Stack>
-)
+const MyKeyResultsPageSwitcherSkeleton = () => {
+  const { colors } = useTheme()
+
+  return (
+    <Stack
+      direction="row"
+      spacing={4}
+      boxShadow={`inset 0 -2px ${colors.black[100] as string}`}
+      pb={4}
+    >
+      <Skeleton w={44} h={21} />
+      <Skeleton w={44} h={21} />
+    </Stack>
+  )
+}
 
 export default MyKeyResultsPageSwitcherSkeleton
