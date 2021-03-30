023c8634c2254cc505bf0bd4091a15a012e10c73
Daniel De Lucca <delucca@pm.me>
2021-03-30 14:19:52 +0000

    chore(tests): adds quarter selector tests

--- src/components/Cycle/Filter/QuarterSelector/quarter-selector.spec.tsx
+++ src/components/Cycle/Filter/QuarterSelector/quarter-selector.spec.tsx
@@ -50,6 +50,7 @@ describe('component lifecycle', () => {
     )
 
     await waitForComponentToPaint(wrapper)
+    await waitForComponentToPaint(wrapper)
 
     const spyAnchor = wrapper.find('RecoilSpyAnchor')
     const loadedFirstCycle = spyAnchor.prop(`CYCLE::FAMILY__"${firstCycle.id}"`)
@@ -88,6 +89,64 @@ describe('component rendering', () => {
       },
     ]
 
+    const TestComponent = ({ filteredYearIDs }: any) => (
+      <MockedProvider mocks={mocks}>
+        <RecoilRoot>
+          <ThemeProvider theme={{}}>
+            <CycleFilterQuarterSelector
+              filteredYearIDs={filteredYearIDs}
+              onQuarterFilter={sinon.fake()}
+            />
+            <RecoilSpy />
+          </ThemeProvider>
+        </RecoilRoot>
+      </MockedProvider>
+    )
+
+    const wrapper = enzyme.mount(<TestComponent filteredYearIDs={parentCycleIDs} />)
+
+    await waitForComponentToPaint(wrapper)
+    await waitForComponentToPaint(wrapper)
+
+    wrapper.setProps({
+      filteredYearIDs: undefined,
+    })
+    wrapper.update()
+
+    const buttons = wrapper.find('CycleFilterQuarterSelectorButton')
+
+    expect(buttons.length).toEqual(4)
+    buttons.map((button) => expect(button.prop('isDisabled')).toEqual(true))
+  })
+
+  it('do not duplicate quarter filter buttons', async () => {
+    const parentCycleIDs = [faker.random.uuid()]
+    const fakePeriod = faker.random.word()
+    const firstCycle = {
+      id: faker.random.uuid(),
+      period: fakePeriod,
+    }
+    const secondCycle = {
+      id: faker.random.uuid(),
+      period: fakePeriod,
+    }
+
+    const mocks = [
+      {
+        request: {
+          query: queries.GET_QUARTERLY_CYCLES_FROM_FILTERED_PARENTS,
+          variables: {
+            parentIds: parentCycleIDs,
+          },
+        },
+        result: {
+          data: {
+            cyclesInSamePeriod: [firstCycle, secondCycle],
+          },
+        },
+      },
+    ]
+
     const wrapper = enzyme.mount(
       <MockedProvider mocks={mocks}>
         <RecoilRoot>
@@ -103,14 +162,11 @@ describe('component rendering', () => {
     )
 
     await waitForComponentToPaint(wrapper)
+    await waitForComponentToPaint(wrapper)
 
-    const spyAnchor = wrapper.find('RecoilSpyAnchor')
-    const loadedFirstCycle = spyAnchor.prop(`CYCLE::FAMILY__"${firstCycle.id}"`)
-    const loadedSecondCycle = spyAnchor.prop(`CYCLE::FAMILY__"${secondCycle.id}"`)
+    const buttons = wrapper.find('CycleFilterQuarterSelectorButton')
 
-    expect(loadedFirstCycle).toBeUndefined()
-    expect(loadedSecondCycle).toBeUndefined()
+    expect(buttons.length).toEqual(1)
+    buttons.map((button) => expect(button.text()).toEqual(fakePeriod))
   })
-
-  it('do not duplicate quarter filter buttons', () => {})
 })
