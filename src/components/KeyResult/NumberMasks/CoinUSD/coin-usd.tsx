650c46f35b31167154df9ff3d7347f5f225e95a0
delucca-pc <delucca+pc@pm.me>
2021-04-01 20:06:32 -0300

    fixing types

--- src/components/KeyResult/NumberMasks/CoinUSD/coin-usd.tsx
+++ src/components/KeyResult/NumberMasks/CoinUSD/coin-usd.tsx
@@ -4,7 +4,7 @@ import Base from 'src/components/KeyResult/NumberMasks/Base'
 import { NumberMaskProperties } from 'src/components/KeyResult/NumberMasks/types'
 
 const CoinUSD = (properties: NumberMaskProperties) => (
-  <Base thousandSeparator="." decimalSeparator="," prefix="$" decimalScale={2} {...properties} />
+  <Base thousandSeparator="." decimalSeparator="," prefix="US$" decimalScale={2} {...properties} />
 )
 
 export default CoinUSD
