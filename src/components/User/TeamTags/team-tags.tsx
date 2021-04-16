e02a3441b23799e7d8a42e00734249f34e75adc4
Daniel De Lucca <delucca@pm.me>
2021-04-16 20:21:17 +0000

    feat(explore): updates back-end contract

--- src/components/User/TeamTags/team-tags.tsx
+++ src/components/User/TeamTags/team-tags.tsx
@@ -1,9 +1,11 @@
 import { Wrap, WrapItem } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
 
 import TeamTag from 'src/components/Team/Tag'
+import { Team } from 'src/components/Team/types'
 import { User } from 'src/components/User/types'
+import { useConnectionEdges } from 'src/state/hooks/useConnectionEdges/hook'
 import { userAtomFamily } from 'src/state/recoil/user'
 
 import UserTeamTagsSkeleton from './skeleton'
@@ -16,11 +18,16 @@ export interface UserTeamTagsProperties {
 
 const UserTeamTags = ({ userID, isLoaded, max }: UserTeamTagsProperties) => {
   const user = useRecoilValue(userAtomFamily(userID))
-  const teams = user?.teams?.slice(0, max)
+  const [teams, setTeamEdges] = useConnectionEdges<Team>()
+  const limitedTeams = teams?.slice(0, max)
 
-  return isLoaded && teams ? (
+  useEffect(() => {
+    if (user) setTeamEdges(user.teams?.edges)
+  }, [user, setTeamEdges])
+
+  return isLoaded && limitedTeams ? (
     <Wrap spacing={2}>
-      {teams.map((team) => (
+      {limitedTeams.map((team) => (
         <WrapItem key={team.id}>
           <TeamTag>{team.name}</TeamTag>
         </WrapItem>
