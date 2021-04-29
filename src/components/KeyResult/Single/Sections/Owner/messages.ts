6d1d63a712cba3962d80bea8cbc03d9c1769e43f
Daniel De Lucca <delucca@pm.me>
2021-04-29 19:56:10 +0000

    bug(key-result): adds hover behavior in owner section

--- src/components/KeyResult/Single/Sections/Owner/messages.ts
+++ src/components/KeyResult/Single/Sections/Owner/messages.ts
@@ -1,11 +1,19 @@
 import { defineMessages } from 'react-intl'
 
-type KeyResultsSectionOwnerMessage = 'label'
+type KeyResultsSectionOwnerMessage = 'label' | 'changeIconDesc'
 
 export default defineMessages<KeyResultsSectionOwnerMessage>({
   label: {
     defaultMessage: 'Responsável',
     id: 'iJq7du',
     description: 'The label text above the Owner section in our key result single page or drawer',
+  },
+
+  changeIconDesc: {
+    defaultMessage:
+      'Um ícone de setas entrelaçadas. Ao clicar aqui você irá trocar o dono deste resultado-chave',
+    id: '4Ekhch',
+    description:
+      'This message is used by screen readers when the user is focusing on the owner section of the key-result drawer',
   },
 })
--- src/components/KeyResult/Single/Sections/Owner/owner.tsx
+++ src/components/KeyResult/Single/Sections/Owner/owner.tsx
@@ -1,9 +1,10 @@
 import { Avatar, Flex, Text, SkeletonCircle, Skeleton, Stack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { useIntl } from 'react-intl'
 import { useRecoilValue } from 'recoil'
 
 import buildSkeletonMinSize from 'lib/chakra/build-skeleton-min-size'
+import SwitchIcon from 'src/components/Icon/Switch'
 import { KeyResult } from 'src/components/KeyResult/types'
 import buildPartialSelector from 'src/state/recoil/key-result/build-partial-selector'
 
@@ -18,21 +19,51 @@ export interface KeyResultSectionOwnerProperties {
 const ownerSelector = buildPartialSelector<KeyResult['owner']>('owner')
 
 const KeyResultSectionOwner = ({ keyResultID }: KeyResultSectionOwnerProperties) => {
+  const [isHovering, setIsHovering] = useState(false)
   const intl = useIntl()
   const owner = useRecoilValue(ownerSelector(keyResultID))
 
   const isOwnerLoaded = Boolean(owner)
 
+  const handleMouseEnter = () => {
+    if (!isHovering) setIsHovering(true)
+  }
+
+  const handleMouseLeave = () => {
+    if (isHovering) setIsHovering(false)
+  }
+
   return (
     <Flex gridGap={2} direction="column">
       <KeyResultSectionHeading>{intl.formatMessage(messages.label)} </KeyResultSectionHeading>
-      <Flex alignItems="center" gridGap={2}>
+      <Flex
+        alignItems="center"
+        gridGap={2}
+        cursor="pointer"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <SkeletonCircle isLoaded={isOwnerLoaded} w={10} h={10}>
-          <Avatar name={owner?.fullName} src={owner?.picture} w={10} h={10} />
+          {isHovering ? (
+            <Flex
+              w={10}
+              h={10}
+              justifyContent="center"
+              alignItems="center"
+              borderColor="brand.500"
+              borderRadius="full"
+              borderWidth={2}
+              borderStyle="dashed"
+            >
+              <SwitchIcon fill="brand.500" desc={intl.formatMessage(messages.changeIconDesc)} />
+            </Flex>
+          ) : (
+            <Avatar name={owner?.fullName} src={owner?.picture} w={10} h={10} />
+          )}
         </SkeletonCircle>
         <Stack spacing={0}>
           <Skeleton isLoaded={isOwnerLoaded} {...buildSkeletonMinSize(isOwnerLoaded, 150, 24)}>
-            <Text color="black.900">{owner?.fullName}</Text>
+            <Text color={isHovering ? 'brand.500' : 'black.900'}>{owner?.fullName}</Text>
           </Skeleton>
 
           <Skeleton isLoaded={isOwnerLoaded} {...buildSkeletonMinSize(isOwnerLoaded, 200, 19)}>
