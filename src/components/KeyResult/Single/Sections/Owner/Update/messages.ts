88f811e6897d6cb959e2ab953b3a3447f2bac32e
Daniel De Lucca <delucca@pm.me>
2021-04-30 17:36:00 +0000

    feat(key-result): adds an empty state to the user list

--- src/components/KeyResult/Single/Sections/Owner/Update/messages.ts
+++ src/components/KeyResult/Single/Sections/Owner/Update/messages.ts
@@ -1,6 +1,6 @@
 import { defineMessages } from 'react-intl'
 
-type KeyResultsSectionOwnerUpdateMessage = 'searchPlaceholder' | 'iconDesc'
+type KeyResultsSectionOwnerUpdateMessage = 'searchPlaceholder' | 'iconDesc' | 'emptyState'
 
 export default defineMessages<KeyResultsSectionOwnerUpdateMessage>({
   searchPlaceholder: {
@@ -15,5 +15,12 @@ export default defineMessages<KeyResultsSectionOwnerUpdateMessage>({
     id: 'UfU6Si',
     description:
       'This text is used by screen readers to explain the icon to the user. It is located in the key-result drawer, inside the edit owner component, as a description for the search icon',
+  },
+
+  emptyState: {
+    defaultMessage: 'Nenhuma pessoa encontrada',
+    id: 'mQBvYY',
+    description:
+      'This message is displayed when the key-result drawer update owner option user list has no users to display. This usually happens when the user tries to search but there is no results',
   },
 })
--- src/components/KeyResult/Single/Sections/Owner/Update/user-list.tsx
+++ src/components/KeyResult/Single/Sections/Owner/Update/user-list.tsx
@@ -1,16 +1,26 @@
-import { Stack } from '@chakra-ui/layout'
+import { Stack, Text } from '@chakra-ui/layout'
 import React from 'react'
+import { useIntl } from 'react-intl'
 
 import { NamedAvatar } from 'src/components/User'
 
 import { KeyResultSingleSectionOwnerUpdateUserListProperties } from './interface'
+import messages from './messages'
 
 export const KeyResultSingleSectionOwnerUpdateUserList = ({
   users,
-}: KeyResultSingleSectionOwnerUpdateUserListProperties) => (
-  <Stack spacing={4}>
-    {users.map((user) => (
-      <NamedAvatar key={user.id} canHover userID={user.id} subtitleType="team" />
-    ))}
-  </Stack>
-)
+}: KeyResultSingleSectionOwnerUpdateUserListProperties) => {
+  const intl = useIntl()
+
+  return (
+    <Stack spacing={4}>
+      {users.length > 0 ? (
+        users.map((user) => (
+          <NamedAvatar key={user.id} canHover userID={user.id} subtitleType="team" />
+        ))
+      ) : (
+        <Text color="black.600">{intl.formatMessage(messages.emptyState)}</Text>
+      )}
+    </Stack>
+  )
+}
