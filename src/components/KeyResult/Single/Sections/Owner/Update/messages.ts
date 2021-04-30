c148b7bd0a9f7fc29bc39cebc0655b95b406e7a5
root <root@bae92d90-5276-4aa6-81c1-b93129d5c3a7.w24w0n2rjk4u1gkdthhdaocy4g.bx.internal.cloudapp.net>
2021-04-30 17:08:51 +0000

    wip

---
+++ src/components/KeyResult/Single/Sections/Owner/Update/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl'
+
+type KeyResultsSectionOwnerUpdateMessage = 'searchPlaceholder'
+
+export default defineMessages<KeyResultsSectionOwnerUpdateMessage>({
+  searchPlaceholder: {
+    defaultMessage: 'Procurar',
+    id: '32yCaj',
+    description:
+      'The text is displayed as a placeholder in our search input inside the key-result drawer while the user is changing the owner of that key-result',
+  },
+})
--- src/components/KeyResult/Single/Sections/Owner/Update/search.tsx
+++ src/components/KeyResult/Single/Sections/Owner/Update/search.tsx
@@ -1,16 +1,26 @@
 import { Input } from '@chakra-ui/input'
 import React, { ChangeEvent } from 'react'
+import { useIntl } from 'react-intl'
 
 import { KeyResultSingleSectionOwnerUpdateSearchProperties } from './interface'
+import messages from './messages'
 
 export const KeyResultSingleSectionOwnerUpdateSearch = ({
   onChange,
 }: KeyResultSingleSectionOwnerUpdateSearchProperties) => {
+  const intl = useIntl()
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
 
     if (onChange) onChange(value)
   }
 
-  return <Input onChange={handleChange} />
+  return (
+    <Input
+      variant="solid"
+      placeholder={intl.formatMessage(messages.searchPlaceholder)}
+      onChange={handleChange}
+    />
+  )
 }
