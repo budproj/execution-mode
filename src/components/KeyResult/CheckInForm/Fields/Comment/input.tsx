1347cdbc1f84f29ead94de670374bc5450ee3af9
delucca <delucca@pm.me>
2021-05-21 10:45:46 -0300

    fixing check-in form

--- src/components/KeyResult/CheckInForm/Fields/Comment/input.tsx
+++ src/components/KeyResult/CheckInForm/Fields/Comment/input.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, FormLabel, Textarea } from '@chakra-ui/react'
 import { useFormikContext } from 'formik'
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { useIntl } from 'react-intl'
 
 import { CheckInFormValues } from 'src/components/KeyResult/CheckInForm/form'
@@ -9,17 +9,26 @@ import messages from './messages'
 
 const CheckInFormFieldCommentInput = () => {
   const intl = useIntl()
+  const [value, setValue] = useState<string | undefined>()
   const { values, setFieldValue } = useFormikContext<CheckInFormValues>()
 
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value)
+  }
+
+  const handleBlur = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setFieldValue('comment', event.target.value)
   }
+
+  useEffect(() => {
+    setValue(values?.comment)
+  }, [values, setValue])
 
   return (
     <Box>
       <FormLabel>{intl.formatMessage(messages.inputLabel)}</FormLabel>
       <Flex direction="column" gridGap={4}>
-        <Textarea value={values.comment} onChange={handleChange} />
+        <Textarea value={value} onChange={handleChange} onBlur={handleBlur} />
       </Flex>
     </Box>
   )
