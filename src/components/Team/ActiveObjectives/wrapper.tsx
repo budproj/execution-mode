e9846868acd993ea70518cf4a6e2997f073bfc98
delucca <delucca@pm.me>
2021-06-01 18:39:01 -0300

    bugfix(pages): fixes team page empty state glitch

--- src/components/Team/ActiveObjectives/wrapper.tsx
+++ src/components/Team/ActiveObjectives/wrapper.tsx
@@ -52,6 +52,7 @@ export const TeamActiveObjectives = ({ teamID }: TeamActiveObjectivesProperties)
 
   const groupedObjectivesByCycle = groupObjectivesByCycle(objectives)
   const isLoaded = called && !loading && groupedObjectivesByCycle
+  const syncedWithLocalState = data?.team.objectives?.edges.length === objectives.length
 
   useEffect(() => {
     if (data) setObjectiveEdges(data?.team.objectives?.edges)
@@ -64,10 +65,10 @@ export const TeamActiveObjectives = ({ teamID }: TeamActiveObjectivesProperties)
   return (
     <Stack spacing={12} h="full">
       {isLoaded ? (
-        !groupedObjectivesByCycle || groupedObjectivesByCycle.length === 0 ? (
+        syncedWithLocalState && groupedObjectivesByCycle?.length === 0 ? (
           <TeamActiveObjectivesEmptyState />
         ) : (
-          groupedObjectivesByCycle.map(([cycle, objectives]) => (
+          groupedObjectivesByCycle?.map(([cycle, objectives]) => (
             <ObjectivesFromCycle
               key={cycle.id}
               cycle={cycle}
