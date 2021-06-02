bdbb2f2921b5c6c880d0ceabc848860394e452d7
delucca <delucca@pm.me>
2021-06-02 16:17:02 -0300

    fixing

--- src/state/recoil/authz/types.ts
+++ src/state/recoil/authz/types.ts
@@ -1,17 +1,12 @@
+import { User } from '@auth0/auth0-react'
+
 import { AUTHZ_ROLES, AUTHZ_TENANT_ROLE } from './constants'
 
 export interface AuthzRolesGroup {
   api: AUTHZ_ROLES[]
 }
 
-export interface AuthzUser {
-  email: string
-  email_verified: boolean
-  name: string
-  nickname: string
-  picture: string
-  sub: string
-  updated_at: Date
+export interface AuthzUser extends User {
   [AUTHZ_TENANT_ROLE.PRODUCTION]?: AUTHZ_ROLES[]
   [AUTHZ_TENANT_ROLE.DEVELOP]?: AUTHZ_ROLES[]
 }
