989573e230d98447daabd14685a966c4808e73cb
delucca <delucca@pm.me>
2021-06-09 18:16:08 -0300

    feature(objective): adds canDelete permission handler

--- src/components/KeyResult/List/Body/Columns/Actions/actions.tsx
+++ src/components/KeyResult/List/Body/Columns/Actions/actions.tsx
@@ -1,13 +1,16 @@
 import { IconButton } from '@chakra-ui/react'
 import React, { ReactElement } from 'react'
 import { useIntl } from 'react-intl'
+import { useRecoilValue } from 'recoil'
 
 import KeyResultListBodyColumnBase, {
   KeyResultListBodyColumnBaseProperties,
 } from 'src/components/KeyResult/List/Body/Columns/Base'
 import { KeyResult } from 'src/components/KeyResult/types'
 
+import { keyResultAtomFamily } from '../../../../../../state/recoil/key-result'
 import { TrashBinOutlineIcon } from '../../../../../Icon/TrashBinOutline/trash-bin-outline'
+import { GraphQLEffect } from '../../../../../types'
 
 import messages from './messages'
 
@@ -19,28 +22,33 @@ export interface KeyResultListBodyColumnActionsProperties
 const KeyResultListBodyColumnActions = ({
   id,
 }: KeyResultListBodyColumnActionsProperties): ReactElement => {
+  const keyResult = useRecoilValue(keyResultAtomFamily(id))
   const intl = useIntl()
+
+  const canDelete = keyResult?.policy?.delete === GraphQLEffect.ALLOW
 
   return (
     <KeyResultListBodyColumnBase preventLineClick justifySelf="flex-end">
-      <IconButton
-        aria-label={intl.formatMessage(messages.deleteIconDesc)}
-        fontSize="lg"
-        w={12}
-        h={12}
-        variant="solid"
-        bg="black.100"
-        color="gray.500"
-        _hover={{
-          bg: 'red.500',
-          color: 'white',
-        }}
-      >
-        <TrashBinOutlineIcon
-          desc={intl.formatMessage(messages.deleteIconDesc)}
-          fill="currentColor"
-        />
-      </IconButton>
+      {canDelete && (
+        <IconButton
+          aria-label={intl.formatMessage(messages.deleteIconDesc)}
+          fontSize="lg"
+          w={12}
+          h={12}
+          variant="solid"
+          bg="black.100"
+          color="gray.500"
+          _hover={{
+            bg: 'red.500',
+            color: 'white',
+          }}
+        >
+          <TrashBinOutlineIcon
+            desc={intl.formatMessage(messages.deleteIconDesc)}
+            fill="currentColor"
+          />
+        </IconButton>
+      )}
     </KeyResultListBodyColumnBase>
   )
 }
