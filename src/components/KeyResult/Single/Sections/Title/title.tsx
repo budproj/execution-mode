4cef5b58a753961f6fc9d4e4aaadf6e094f19cc1
delucca <delucca@pm.me>
2021-07-15 14:09:37 +0000

    feat(kr): finishes progress bar

--- src/components/KeyResult/Single/Sections/Title/title.tsx
+++ src/components/KeyResult/Single/Sections/Title/title.tsx
@@ -1,14 +1,17 @@
 import { useMutation } from '@apollo/client'
 import { Box, Flex, Skeleton, SkeletonText, Spinner, Stack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useIntl } from 'react-intl'
 import { useRecoilState, useRecoilValue } from 'recoil'
 
 import EditableInputValue from 'src/components/Base/EditableInputValue'
 import LastUpdateText from 'src/components/Base/LastUpdateText'
 import { KeyResultDynamicIcon } from 'src/components/KeyResult'
+import PercentageMask from 'src/components/KeyResult/NumberMasks/Percentage'
+import ProgressSlider from 'src/components/KeyResult/ProgressSlider'
 import { KeyResult } from 'src/components/KeyResult/types'
 import { GraphQLEffect } from 'src/components/types'
+import useConfidenceTag from 'src/state/hooks/useConfidenceTag'
 import { keyResultAtomFamily } from 'src/state/recoil/key-result'
 import selectLatestCheckIn from 'src/state/recoil/key-result/check-in/latest'
 
@@ -27,6 +30,8 @@ const KeyResultSectionTitle = ({ keyResultID }: KeyResultSectionTitleProperties)
   const [keyResult, setKeyResult] = useRecoilState(keyResultAtomFamily(keyResultID))
   const latestCheckIn = useRecoilValue(selectLatestCheckIn(keyResultID))
   const intl = useIntl()
+  const [confidenceTag, setConfidence] = useConfidenceTag(keyResult?.status?.confidence)
+
   const [updateKeyResult, { loading }] = useMutation<UpdateKeyResultTitleMutationResult>(
     queries.UPDATE_KEY_RESULT_TITLE,
   )
@@ -53,6 +58,10 @@ const KeyResultSectionTitle = ({ keyResultID }: KeyResultSectionTitleProperties)
     })
   }
 
+  useEffect(() => {
+    if (keyResult?.status?.confidence) setConfidence(keyResult.status.confidence)
+  }, [keyResult, setConfidence])
+
   return (
     <Flex gridGap={4} alignItems="flex-start">
       <Skeleton borderRadius={10} isLoaded={isLoaded}>
@@ -65,7 +74,7 @@ const KeyResultSectionTitle = ({ keyResultID }: KeyResultSectionTitleProperties)
         />
       </Skeleton>
 
-      <Stack spacing={0} flexGrow={1}>
+      <Stack spacing={1} flexGrow={1}>
         <Stack direction="row">
           <Skeleton isLoaded={isLoaded} flexGrow={1}>
             <EditableInputValue
@@ -104,6 +113,16 @@ const KeyResultSectionTitle = ({ keyResultID }: KeyResultSectionTitleProperties)
             prefix={intl.formatMessage(messages.lastUpdateTextPrefix)}
           />
         </SkeletonText>
+
+        <Stack spacing={4} direction="row">
+          <Skeleton isLoaded={isLoaded} flexGrow={1}>
+            <ProgressSlider isDisabled id={keyResult?.id} isActive={isActive} />
+          </Skeleton>
+
+          <Skeleton noOfLines={1} isLoaded={isLoaded} color="gray.400" fontWeight={700}>
+            <PercentageMask value={keyResult?.status?.progress} displayType="text" />
+          </Skeleton>
+        </Stack>
       </Stack>
     </Flex>
   )
