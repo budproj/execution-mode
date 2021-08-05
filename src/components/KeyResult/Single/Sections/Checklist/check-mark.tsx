316d8c7d465da711046f5f2377d4647b59d13519
delucca <delucca@protonmail.com>
2021-08-05 16:12:39 -0300

    feat(kr): turns checkbox label into editable

--- src/components/KeyResult/Single/Sections/Checklist/check-mark.tsx
+++ src/components/KeyResult/Single/Sections/Checklist/check-mark.tsx
@@ -1,10 +1,23 @@
-import { Checkbox, Skeleton } from '@chakra-ui/react'
+import {
+  Checkbox,
+  Stack,
+  Editable,
+  EditablePreview,
+  EditableInput,
+  Skeleton,
+} from '@chakra-ui/react'
 import React from 'react'
 
 import { KeyResultCheckMark as KeyResultCheckMarkType } from '../../../../../components/KeyResult/types'
 
 export const KeyResultCheckMark = ({ id, description }: Partial<KeyResultCheckMarkType>) => (
-  <Skeleton isLoaded={Boolean(id)}>
-    <Checkbox>{description}</Checkbox>
+  <Skeleton isLoaded={Boolean(id)} w="full">
+    <Stack direction="row" alignItems="center">
+      <Checkbox />
+      <Editable value={description} w="full">
+        <EditablePreview />
+        <EditableInput />
+      </Editable>
+    </Stack>
   </Skeleton>
 )
