a85b7aca5c4162e84ed62a2c68588ef67cfc08f8
delucca <delucca@protonmail.com>
2021-08-05 17:09:50 -0300

    bugfix: removes unused intl

--- src/components/KeyResult/Single/Sections/Checklist/checklist.tsx
+++ src/components/KeyResult/Single/Sections/Checklist/checklist.tsx
@@ -1,6 +1,5 @@
 import { Stack } from '@chakra-ui/layout'
 import React from 'react'
-import { useIntl } from 'react-intl'
 
 import { KeyResultCheckMark as KeyResultCheckMarkType } from 'src/components/KeyResult/types'
 
@@ -17,15 +16,11 @@ export const KeyResultChecklist = ({
   nodes,
   refresh,
   keyResultID,
-}: KeyResultChecklistProperties) => {
-  const intl = useIntl()
-
-  return (
-    <Stack alignItems="flex-start">
-      {nodes.map((node) => (
-        <KeyResultCheckMark key={node.id} node={node} refresh={refresh} />
-      ))}
-      {nodes.length > 0 && <NewCheckMark refresh={refresh} keyResultID={keyResultID} />}
-    </Stack>
-  )
-}
+}: KeyResultChecklistProperties) => (
+  <Stack alignItems="flex-start">
+    {nodes.map((node) => (
+      <KeyResultCheckMark key={node.id} node={node} refresh={refresh} />
+    ))}
+    {nodes.length > 0 && <NewCheckMark refresh={refresh} keyResultID={keyResultID} />}
+  </Stack>
+)
