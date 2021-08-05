9b051efabd28ab7ff66cd1c4a07d1a2cea0810e5
delucca <delucca@protonmail.com>
2021-08-05 16:56:39 -0300

    feat(kr): adds insert check-mark button layout

--- src/components/KeyResult/Single/Sections/Checklist/checklist.tsx
+++ src/components/KeyResult/Single/Sections/Checklist/checklist.tsx
@@ -1,19 +1,44 @@
 import { Stack } from '@chakra-ui/layout'
+import { Button } from '@chakra-ui/react'
 import React from 'react'
+import { useIntl } from 'react-intl'
 
+import { PlusOutline } from 'src/components/Icon'
 import { KeyResultCheckMark as KeyResultCheckMarkType } from 'src/components/KeyResult/types'
 
 import { KeyResultCheckMark } from './check-mark'
+import messages from './messages'
 
 interface KeyResultChecklistProperties {
   nodes: KeyResultCheckMarkType[]
   refresh: () => void
 }
 
-export const KeyResultChecklist = ({ nodes, refresh }: KeyResultChecklistProperties) => (
-  <Stack>
-    {nodes.map((node) => (
-      <KeyResultCheckMark key={node.id} node={node} refresh={refresh} />
-    ))}
-  </Stack>
-)
+export const KeyResultChecklist = ({ nodes, refresh }: KeyResultChecklistProperties) => {
+  const intl = useIntl()
+
+  return (
+    <Stack alignItems="flex-start">
+      {nodes.map((node) => (
+        <KeyResultCheckMark key={node.id} node={node} refresh={refresh} />
+      ))}
+      {nodes.length > 0 && (
+        <Button
+          variant="text"
+          pl={0}
+          colorScheme="brand"
+          leftIcon={
+            <PlusOutline
+              desc={intl.formatMessage(messages.newCheckMarkButtonIconDescription)}
+              stroke="currentColor"
+              fill="currentColor"
+              fontSize="xl"
+            />
+          }
+        >
+          {intl.formatMessage(messages.newCheckMarkButtonLabel)}
+        </Button>
+      )}
+    </Stack>
+  )
+}
--- src/components/KeyResult/Single/Sections/Checklist/messages.ts
+++ src/components/KeyResult/Single/Sections/Checklist/messages.ts
@@ -1,11 +1,28 @@
 import { defineMessages } from 'react-intl'
 
-type KeyResultsSectionChecklistMessage = 'heading'
+type KeyResultsSectionChecklistMessage =
+  | 'heading'
+  | 'newCheckMarkButtonLabel'
+  | 'newCheckMarkButtonIconDescription'
 
 export default defineMessages<KeyResultsSectionChecklistMessage>({
   heading: {
     defaultMessage: 'Check-list',
     id: 'PktsGq',
     description: 'This is the title of our checklist section inside the key-result sidebar',
+  },
+
+  newCheckMarkButtonLabel: {
+    defaultMessage: 'Adicionar novo item',
+    id: 'zennqC',
+    description: 'This label appears in our key-result sidebar drawer, in the end of our checklist',
+  },
+
+  newCheckMarkButtonIconDescription: {
+    defaultMessage:
+      'Um ícone do sinal de positivo que, ao clicar, irá adicionar um novo item na checklist',
+    id: 'e2FL3I',
+    description:
+      'This is used by screen readers to understand the insert new check mark icon in our key-result drawer',
   },
 })
