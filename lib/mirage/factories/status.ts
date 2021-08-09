f55b4eb8aab49288df49d4b54b9f48255612e773
Victor Perin <me@victorperin.ninja>
2021-08-09 13:42:23 -0300

    refactor(mirage/factories/status): use status faker from 0 to 100

--- lib/mirage/factories/status.ts
+++ lib/mirage/factories/status.ts
@@ -2,7 +2,7 @@ import faker from 'faker'
 import { Factory } from 'miragejs'
 
 export const status = Factory.extend({
-  progress: () => faker.datatype.number({ min: 40, max: 100 }),
+  progress: () => faker.datatype.number({ min: 0, max: 100 }),
   confidence: () => faker.datatype.number({ min: 0, max: 100 }),
   isActive: true,
   isOutdated: false,
