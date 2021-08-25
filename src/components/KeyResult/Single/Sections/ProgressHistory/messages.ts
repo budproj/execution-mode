38618c6ec4e5b7e507af00ee555994e32d862eaf
delucca <delucca@protonmail.com>
2021-08-25 20:54:28 +0000

    feat(key-result): finishes styling chart

---
+++ src/components/KeyResult/Single/Sections/ProgressHistory/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl'
+
+type ProgressHistoryMessages = 'metricName'
+
+export default defineMessages<ProgressHistoryMessages>({
+  metricName: {
+    defaultMessage: 'Progresso',
+    id: 'QRn560',
+    description:
+      'This text is used inside our key-result sidebar, in the chart, as the metric name of a key-result progress',
+  },
+})
--- src/components/KeyResult/Single/Sections/ProgressHistory/wrapper.tsx
+++ src/components/KeyResult/Single/Sections/ProgressHistory/wrapper.tsx
@@ -1,6 +1,7 @@
 import { Box, useToken } from '@chakra-ui/react'
 import format from 'date-fns/format'
 import React from 'react'
+import { useIntl } from 'react-intl'
 import {
   Area,
   Tooltip,
@@ -10,31 +11,52 @@ import {
   YAxis,
   CartesianGrid,
 } from 'recharts'
+import { Payload } from 'recharts/types/component/DefaultTooltipContent'
+
+import messages from './messages'
+
+const formatData = (value: number) => value * 100
+
+const formatDate = (rawDate?: string) => {
+  const date = rawDate && rawDate !== '' ? new Date(rawDate) : new Date()
+
+  return format(date, 'dd/MM')
+}
+
+const formatLabel = (_: unknown, [data]: Array<Payload<string, string>>) => {
+  return formatDate(data?.payload?.date)
+}
 
 export const ProgressHistoryChart = () => {
   const fakeData = [
     {
-      time: 1616889600,
-      close: 20,
+      id: '027acd9a-844c-4817-ae02-206c62a62b80',
+      progress: 0.2,
+      date: '2021-03-16T14:47:48.000Z',
     },
     {
-      time: 1617148800,
-      close: 40,
+      id: '027acd9a-844c-4817-ae02-206c62a62b80',
+      progress: 0.7,
+      date: '2021-04-16T14:47:48.000Z',
     },
     {
-      time: 1617408000,
-      close: 30,
+      id: '027acd9a-844c-4817-ae02-206c62a62b80',
+      progress: 0.5,
+      date: '2021-05-16T14:47:48.000Z',
     },
     {
-      time: 1617667200,
-      close: 40,
+      id: '027acd9a-844c-4817-ae02-206c62a62b80',
+      progress: 0.5,
+      date: '2021-06-16T14:47:48.000Z',
     },
     {
-      time: 1617667900,
-      close: 40,
+      id: '027acd9a-844c-4817-ae02-206c62a62b80',
+      progress: 1,
+      date: '2021-07-16T14:47:48.000Z',
     },
   ]
 
+  const intl = useIntl()
   const [brand100, brand300, brand500, newGray200, newGray400, newGray500] = useToken('colors', [
     'brand.100',
     'brand.300',
@@ -43,8 +65,6 @@ export const ProgressHistoryChart = () => {
     'new-gray.400',
     'new-gray.500',
   ])
-
-  const convertDate = (timestamp: any) => format(new Date(timestamp * 1000), 'dd/MM')
 
   return (
     <Box w="full" h={48}>
@@ -58,25 +78,12 @@ export const ProgressHistoryChart = () => {
           </defs>
 
           <CartesianGrid vertical={false} stroke={newGray200} />
-
-          <Area
-            yAxisId="primary"
-            xAxisId="primary"
-            type="monotone"
-            unit="%"
-            dataKey="close"
-            strokeWidth={2}
-            stroke={brand500}
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-
-          <Tooltip />
+          <Tooltip separator=": " formatter={formatData} labelFormatter={formatLabel} />
 
           <XAxis
             xAxisId="primary"
             dataKey="time"
-            tickFormatter={convertDate}
+            tickFormatter={formatDate}
             stroke={newGray400}
             tickLine={false}
             tick={{ fill: newGray500 }}
@@ -87,12 +94,25 @@ export const ProgressHistoryChart = () => {
           <YAxis
             yAxisId="primary"
             type="number"
-            ticks={[25, 50, 75, 100]}
+            ticks={[0.25, 0.5, 0.75, 1]}
             tickLine={false}
             stroke={newGray400}
             width={1}
           />
           <YAxis orientation="right" stroke={newGray400} width={1} tickLine={false} />
+
+          <Area
+            yAxisId="primary"
+            xAxisId="primary"
+            type="monotone"
+            unit="%"
+            dataKey="progress"
+            name={intl.formatMessage(messages.metricName)}
+            strokeWidth={2}
+            stroke={brand500}
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
         </ComposedChart>
       </ResponsiveContainer>
     </Box>
