76e0b77d29046026fcf5bafde49eea2bd8bba058
delucca <delucca@protonmail.com>
2021-09-10 12:58:36 +0000

    bugfix(mentions): fixes overlapping send button

--- src/components/KeyResult/Single/Sections/AddComment/input.tsx
+++ src/components/KeyResult/Single/Sections/AddComment/input.tsx
@@ -99,6 +99,7 @@ const KeyResultSectionAddCommentInput = ({
       borderRadius={numberOfRows === 1 ? 'full' : 60 / numberOfRows}
       px={4}
       py={2}
+      pr={10}
     >
       <MentionsInput
         allowSuggestionsAboveCursor
@@ -176,7 +177,7 @@ const KeyResultSectionAddCommentInput = ({
         type="submit"
         position="absolute"
         right={6}
-        bottom={4}
+        bottom="1.2rem"
         color="gray.200"
         aria-label={intl.formatMessage(messages.paperPlaneIconDesc)}
         _hover={{
