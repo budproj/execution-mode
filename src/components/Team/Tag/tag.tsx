39c71432524d21e53f6d02c4fdcc24b1959b8027
delucca <delucca@protonmail.com>
2021-09-20 21:30:01 +0000

    feat(user): adds popover and fixes shadow

--- src/components/Team/Tag/tag.tsx
+++ src/components/Team/Tag/tag.tsx
@@ -1,17 +1,27 @@
 import { Tag, TagProps } from '@chakra-ui/react'
-import React from 'react'
+import React, { forwardRef, RefObject } from 'react'
 
-const TeamTag = (properties: TagProps) => (
-  <Tag
-    bg="new-gray.300"
-    color="gray.500"
-    textTransform="uppercase"
-    fontWeight={500}
-    fontSize="sm"
-    borderRadius={4}
-    p={2}
-    {...properties}
-  />
+const TeamTag = forwardRef(
+  (
+    properties: TagProps,
+    reference:
+      | string
+      | ((instance: HTMLDivElement | null) => void)
+      | RefObject<HTMLDivElement>
+      | null,
+  ) => (
+    <Tag
+      ref={reference}
+      bg="new-gray.300"
+      color="gray.500"
+      textTransform="uppercase"
+      fontWeight={500}
+      fontSize="sm"
+      borderRadius={4}
+      p={2}
+      {...properties}
+    />
+  ),
 )
 
 export default TeamTag
--- src/components/User/Teams/add-team.tsx
+++ src/components/User/Teams/add-team.tsx
@@ -1,15 +1,21 @@
+import { Popover, PopoverContent, PopoverTrigger } from '@chakra-ui/react'
 import React from 'react'
 
 import TeamTag from 'src/components/Team/Tag'
 
 export const AddUserTeam = () => (
-  <TeamTag
-    p={3}
-    py={2}
-    cursor="pointer"
-    _hover={{ bg: 'brand.500', color: 'white' }}
-    transition="all .3s"
-  >
-    +
-  </TeamTag>
+  <Popover placement="bottom-end">
+    <PopoverTrigger>
+      <TeamTag
+        p={3}
+        py={2}
+        cursor="pointer"
+        _hover={{ bg: 'brand.500', color: 'white' }}
+        transition="all .3s"
+      >
+        +
+      </TeamTag>
+    </PopoverTrigger>
+    <PopoverContent>Ok</PopoverContent>
+  </Popover>
 )
