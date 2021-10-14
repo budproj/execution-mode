c1a304ede30d5d4d573c77bee9d276a98d43e580
Victor Perin <me@victorperin.ninja>
2021-10-14 19:13:41 -0300

    feat(components/keyresult/single/owner): [WIP] add popover content

--- src/components/KeyResult/Single/Sections/Owner/support-team-popover.tsx
+++ src/components/KeyResult/Single/Sections/Owner/support-team-popover.tsx
@@ -1,37 +1,57 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, PopoverFooter, PopoverHeader, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { NamedAvatar } from "src/components/User";
+import { AllReachableUsers } from "src/components/User/AllReachableUsers/wrapper";
 import { User } from "src/components/User/types";
 
 const HelpIcon = () => <>?</>
+const BackIcon = () => <>?</>
 
 type SupportTeamPopoverProperties = {
   supportTeamMembers?: User[]
 }
 
+type AddNewMemberProperties = {
+  handleUserSelect: (userID: string) => void
+  toggleIsAdding: () => void
+}
+
+const AddNewMember = ({ handleUserSelect, toggleIsAdding }: AddNewMemberProperties) => <Box>
+  <PopoverHeader onClick={toggleIsAdding}>
+    <BackIcon />
+    <Text as="span" fontSize='lg' color="new-gray.900" fontWeight="400">Time de Apoio</Text>
+  </PopoverHeader>
+  <AllReachableUsers onSelect={handleUserSelect} />
+</Box>
+
 export const SupportTeamPopover = ({ supportTeamMembers }: SupportTeamPopoverProperties) => {
   const [isAdding, setIsAdding] = useState<boolean>(false);
-  const setToAdding = () => setIsAdding(true)
-  const setToNotAdding = () => setIsAdding(false)
+  const toggleIsAdding = () => setIsAdding(!isAdding)
+
+  const handleUserSelect = () => {
+    toggleIsAdding()
+  }
+
+  const owner = 'Aline'
 
   return isAdding ?
-    <Box></Box> :
+    <AddNewMember handleUserSelect={handleUserSelect} toggleIsAdding={toggleIsAdding}/> :
     <Box>
-      <Box>
+      <PopoverHeader>
         <Text as="span" fontSize='xl' color="new-gray.900" fontWeight="400">Time de Apoio</Text><HelpIcon/>
-      </Box>
+        <Text color="new-gray.600">Essas são as pessoas ajudando {owner} neste resultado-chave:</Text>
+      </PopoverHeader>
       <Box>
         {supportTeamMembers?.map( (member) => (
           <NamedAvatar
-            canHover
-            nameColor="black.900"
+            nameColor="new-gray.800"
             subtitleType="role"
             userID={member.id.toString()}
           />
         ))}
       </Box>
-      <Box onClick={setToAdding}>
+      <PopoverFooter onClick={toggleIsAdding}>
         <Text>Adicionar pessoa</Text>
-      </Box>
+      </PopoverFooter>
     </Box>
 }
