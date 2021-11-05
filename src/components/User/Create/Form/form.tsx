d96c035e1c48d55763bcbefdce1ca4b52638df6b
delucca <delucca@protonmail.com>
2021-11-05 20:08:36 +0000

    feat(user): adds text fields

--- src/components/User/Create/Form/form.tsx
+++ src/components/User/Create/Form/form.tsx
@@ -1,8 +1,9 @@
+import { Button } from '@chakra-ui/button'
 import { FormControl, FormLabel } from '@chakra-ui/form-control'
 import { Input } from '@chakra-ui/input'
-import { Stack } from '@chakra-ui/layout'
-import { Field, Form, Formik, FormikHelpers } from 'formik'
-import React from 'react'
+import { HStack, Stack } from '@chakra-ui/layout'
+import { Field, Form, Formik, FormikHelpers, useFormikContext } from 'formik'
+import React, { ChangeEvent } from 'react'
 import { useIntl } from 'react-intl'
 
 import { USER_GENDER } from '../../constants'
@@ -47,16 +48,30 @@ export const CreateUserForm = ({ initialValues, onSubmit }: CreateUserFormProper
 
   return (
     <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      <Form>
-        <FormControl>
-          <Stack>
-            <CreateUserTextField
-              id="firstName"
-              label={intl.formatMessage(messages.firstNameLabel)}
-            />
-          </Stack>
-        </FormControl>
-      </Form>
+      {({ values }) => (
+        <Form style={{ height: '100%' }}>
+          <FormControl h="full">
+            <Stack spacing={8} h="full">
+              <CreateUserTextField
+                id="firstName"
+                label={intl.formatMessage(messages.firstNameLabel)}
+              />
+              <CreateUserTextField
+                id="lastName"
+                label={intl.formatMessage(messages.lastNameLabel)}
+              />
+              <CreateUserTextField id="email" label={intl.formatMessage(messages.emailLabel)} />
+              <CreateUserTextField id="role" label={intl.formatMessage(messages.roleLabel)} />
+
+              <HStack flexGrow={1} alignItems="flex-end">
+                <Button variant="solid" colorScheme="brand" type="submit">
+                  {intl.formatMessage(messages.submitButtonLabel, { gender: values.gender })}
+                </Button>
+              </HStack>
+            </Stack>
+          </FormControl>
+        </Form>
+      )}
     </Formik>
   )
 }
@@ -66,9 +81,17 @@ type CreateUserTextField = {
   label: string
 }
 
-const CreateUserTextField = ({ id, label }: CreateUserTextField) => (
-  <Stack spacing={0}>
-    <FormLabel>{label}</FormLabel>
-    <Field name={id} component={Input} />
-  </Stack>
-)
+const CreateUserTextField = ({ id, label }: CreateUserTextField) => {
+  const { setFieldValue } = useFormikContext<CreateUserFormValues>()
+
+  const handleBlur = (event: ChangeEvent<HTMLInputElement>) => {
+    setFieldValue(id, event.target.value)
+  }
+
+  return (
+    <Stack spacing={0}>
+      <FormLabel>{label}</FormLabel>
+      <Field name={id} component={Input} onBlur={handleBlur} />
+    </Stack>
+  )
+}
--- src/components/User/Create/Form/messages.ts
+++ src/components/User/Create/Form/messages.ts
@@ -1,11 +1,61 @@
 import { defineMessages } from 'react-intl'
 
-type CreateUserFormMessage = 'firstNameLabel'
+type CreateUserFormMessage =
+  | 'firstNameLabel'
+  | 'lastNameLabel'
+  | 'emailLabel'
+  | 'roleLabel'
+  | 'genderLabel'
+  | 'teamLabel'
+  | 'pictureLabel'
+  | 'submitButtonLabel'
 
 export default defineMessages<CreateUserFormMessage>({
   firstNameLabel: {
     defaultMessage: 'Nome',
-    id: 'V4ahTq',
-    description: 'The label of the first option in the create user form',
+    id: 'PSCS+h',
+    description: 'The label of the first name field in the create user form',
+  },
+
+  lastNameLabel: {
+    defaultMessage: 'Sobrenome',
+    id: 'yw+DGB',
+    description: 'The label of the last name field in the create user form',
+  },
+
+  emailLabel: {
+    defaultMessage: 'Email',
+    id: 'eVsyB5',
+    description: 'The label of the email field in the create user form',
+  },
+
+  roleLabel: {
+    defaultMessage: 'Cargo',
+    id: 'PxWBQZ',
+    description: 'The label of the role field in the create user form',
+  },
+
+  genderLabel: {
+    defaultMessage: 'Gênero',
+    id: 'SL45P4',
+    description: 'The label of the gender field in the create user form',
+  },
+
+  teamLabel: {
+    defaultMessage: 'Time',
+    id: 'SRIfUY',
+    description: 'The label of the team field in the create user form',
+  },
+
+  pictureLabel: {
+    defaultMessage: 'Foto',
+    id: 'cz6EUM',
+    description: 'The label of the picture field in the create user form',
+  },
+
+  submitButtonLabel: {
+    defaultMessage: 'Criar usuári{gender, select, MALE {o} FEMALE {a} other {o}}',
+    id: 'xw3Bb5',
+    description: 'The label of the submit button of our form',
   },
 })
