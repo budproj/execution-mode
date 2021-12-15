b464bb86c5ed018e69293d4f42c56dc0b093ac22
Victor Perin <me@victorperin.ninja>
2021-12-15 14:17:39 -0300

    feat(amplitude): allow and capture utm tags in url (#199)

--- src/state/hooks/useAmplitude/ensure-initialized.ts
+++ src/state/hooks/useAmplitude/ensure-initialized.ts
@@ -2,9 +2,16 @@ import { AmplitudeClient } from 'amplitude-js'
 
 import getConfig from '../../../config'
 
+const defaultAmplitudeSettings = {
+  saveEvents: true,
+  includeUtm: true,
+  includeReferer: true,
+  saveParamsReferrerOncePerSession: false,
+}
+
 export const ensureInitialized = (client: AmplitudeClient): void => {
   const { publicRuntimeConfig } = getConfig()
   const { apiKey } = publicRuntimeConfig.amplitude
 
-  client.init(apiKey)
+  client.init(apiKey, undefined, defaultAmplitudeSettings)
 }
