32790cc87c411ea91b338b63f24abdba2ad98cb6
Victor Perin <me@victorperin.ninja>
2021-12-16 22:46:43 -0300

    bugfix(pages): auth0 correct redirectUri (#201)
    * bugfix(pages): auth0 correct redirectUri
    
    * bugfix(pages): auth0 correct redirectUri
    
    * bugfix(pages): auth0 correct redirectUri

--- src/components/Base/AuthzGatekeeper/authz-gatekeeper.tsx
+++ src/components/Base/AuthzGatekeeper/authz-gatekeeper.tsx
@@ -1,27 +1,17 @@
-import { useAuth0 } from '@auth0/auth0-react'
+import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import React, { ReactElement, useEffect } from 'react'
 import { useSetRecoilState } from 'recoil'
 
-import logger from 'lib/logger'
 import PageLoading from 'src/components/Base/PageLoading'
 import authzAtom from 'src/state/recoil/authz/atom'
-
-const component = 'AuthzGatekeeper'
 
 export interface AuthzGatekeeperProperties {
   children: ReactElement
 }
 
 const AuthzGatekeeper = ({ children }: AuthzGatekeeperProperties): ReactElement => {
-  const { isLoading, isAuthenticated, loginWithRedirect, user } = useAuth0()
+  const { isLoading, isAuthenticated, user } = useAuth0()
   const setAuthzUser = useSetRecoilState(authzAtom)
-
-  useEffect(() => {
-    if (!isLoading && !isAuthenticated)
-      loginWithRedirect().catch((error) =>
-        logger.error('Could not redirect user to auth. Reason:', { data: error, component }),
-      )
-  }, [isLoading, isAuthenticated, loginWithRedirect])
 
   useEffect(() => {
     setAuthzUser(user)
@@ -30,4 +20,4 @@ const AuthzGatekeeper = ({ children }: AuthzGatekeeperProperties): ReactElement 
   return isLoading || !isAuthenticated ? <PageLoading /> : children
 }
 
-export default AuthzGatekeeper
+export default withAuthenticationRequired(AuthzGatekeeper)
