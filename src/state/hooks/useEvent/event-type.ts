bceeafe2c0712f2dd5266b786f6461c026ebd664
delucca <d.pgp@fonsecafloriano.com>
2022-02-10 16:31:52 -0300

    feat(events): adds open checklist event

--- src/state/hooks/useEvent/event-type.ts
+++ src/state/hooks/useEvent/event-type.ts
@@ -3,4 +3,5 @@ export enum EventType {
   KEY_RESULT_PROGRESS_CHART_VIEW = 'KeyResultProgressChartView',
   KEY_RESULT_CREATE_CHECKLIST = 'KeyResultCreateCheckList',
   OPENED_KEY_RESULT_DRAWER = 'OpenedKeyResultDrawer',
+  OPENED_KEY_RESULT_CHECKLIST = 'OpenedKeyResultChecklist',
 }
---
+++ src/state/hooks/useEvent/events/opened-key-result-checklist.ts
@@ -0,0 +1,5 @@
+import { BaseEventData } from './base-event'
+
+export interface OpenedKeyResultChecklistEventData extends BaseEventData {
+  keyResultID: string
+}
