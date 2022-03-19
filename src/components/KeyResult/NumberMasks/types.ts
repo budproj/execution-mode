a254da9c933c46c38cf1d53ee7e401091e61632c
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-03-18 22:35:13 -0300

    fix(build)

--- src/components/KeyResult/NumberMasks/types.ts
+++ src/components/KeyResult/NumberMasks/types.ts
@@ -4,4 +4,10 @@ import { NumberFormatProps } from 'react-number-format'
 export interface NumberMaskProperties extends NumberFormatProps {
   handleChange?: (value: NumberFormatProps['value']) => void
   isDisabled?: InputProps['isDisabled']
+  bg?: string
+  fontSize?: string | number
+  _disabled?: {
+    opacity: number
+    borderColor: string
+  }
 }
