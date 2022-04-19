8474e26b3f0bb7a766809388230170502c58393d
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-04-19 10:36:17 -0300

    chore(key-result/owner): return option to render owners name and role

--- src/components/Objective/Accordion/Item/Panel/key-results.tsx
+++ src/components/Objective/Accordion/Item/Panel/key-results.tsx
@@ -61,7 +61,7 @@ export const ObjectiveKeyResults = ({
   const isEditing = mode === ObjectiveMode.EDIT
 
   const handleLineClick = (id: KeyResult['id']) => setOpenDrawer(id)
-  const templateColumns = isEditing ? '2fr 1fr 80px 60px' : '2fr 1fr 80px'
+  const templateColumns = isEditing ? '2fr 1fr 0.8fr 60px' : '2fr 1fr 0.8fr'
   const columns = without(
     [
       KEY_RESULT_LIST_COLUMN.KEY_RESULT,
@@ -124,6 +124,10 @@ export const ObjectiveKeyResults = ({
           withConfidenceTag: true,
           isActive: !isDisabled,
           isDisabled,
+        },
+        [KEY_RESULT_LIST_COLUMN.OWNER]: {
+          displayName: true,
+          displayRole: true,
         },
         [KEY_RESULT_LIST_COLUMN.ACTIONS]: {
           onDelete: handleKeyResultDelete,
