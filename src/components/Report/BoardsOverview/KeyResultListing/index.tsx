bdcc79850eb08ab7e260335f17f5014e48a9aabb
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-05-02 16:55:26 -0300

    fix(strategy-health): add max height to kr list modal

--- src/components/Report/BoardsOverview/KeyResultListing/index.tsx
+++ src/components/Report/BoardsOverview/KeyResultListing/index.tsx
@@ -30,6 +30,22 @@ const StyledModal = styled(ModalContent)`
   max-width: 1200px;
 `
 
+const StyledTableWrapper = styled(Flex)`
+  max-height: calc(100vh - 200px);
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #cecece;
+    border: 0px none #ffffff;
+    border-radius: 50px;
+  }
+`
+
 export const KeyResultListingModal = ({
   isOpen,
   confidence,
@@ -62,49 +78,52 @@ export const KeyResultListingModal = ({
             />
           </Flex>
 
-          <KeyResultList
-            type={KEY_RESULT_LIST_TYPE.STATIC}
-            keyResultIDs={keyResultIds}
-            isLoading={loading}
-            templateColumns="1.5fr 1fr 100px 80px 1fr"
-            borderColor="new-gray.400"
-            headProperties={{
-              [KEY_RESULT_LIST_COLUMN.KEY_RESULT]: {
-                hidden: false,
-              },
-              [KEY_RESULT_LIST_COLUMN.PROGRESS]: {
-                hidden: false,
-              },
-              [KEY_RESULT_LIST_COLUMN.TEAM]: {
-                hidden: false,
-              },
-              [KEY_RESULT_LIST_COLUMN.OWNER]: {
-                hidden: false,
-              },
-              [KEY_RESULT_LIST_COLUMN.OBJECTIVE]: {
-                hidden: false,
-              },
-            }}
-            columns={[
-              KEY_RESULT_LIST_COLUMN.KEY_RESULT,
-              KEY_RESULT_LIST_COLUMN.PROGRESS,
-              KEY_RESULT_LIST_COLUMN.TEAM,
-              KEY_RESULT_LIST_COLUMN.OWNER,
-              KEY_RESULT_LIST_COLUMN.OBJECTIVE,
-            ]}
-            bodyProperties={{
-              [KEY_RESULT_LIST_COLUMN.KEY_RESULT]: {
-                withDynamicIcon: true,
-                withLastUpdateInfo: true,
-              },
-              [KEY_RESULT_LIST_COLUMN.PROGRESS]: {
-                withConfidenceTag: true,
-              },
-              [KEY_RESULT_LIST_COLUMN.OWNER]: {
-                justifyContent: 'flex-end',
-              },
-            }}
-          />
+          <StyledTableWrapper>
+            <KeyResultList
+              type={KEY_RESULT_LIST_TYPE.STATIC}
+              keyResultIDs={keyResultIds}
+              isLoading={loading}
+              templateColumns="1.5fr 1fr 100px 80px 1fr"
+              borderColor="new-gray.400"
+              flex="1"
+              headProperties={{
+                [KEY_RESULT_LIST_COLUMN.KEY_RESULT]: {
+                  hidden: false,
+                },
+                [KEY_RESULT_LIST_COLUMN.PROGRESS]: {
+                  hidden: false,
+                },
+                [KEY_RESULT_LIST_COLUMN.TEAM]: {
+                  hidden: false,
+                },
+                [KEY_RESULT_LIST_COLUMN.OWNER]: {
+                  hidden: false,
+                },
+                [KEY_RESULT_LIST_COLUMN.OBJECTIVE]: {
+                  hidden: false,
+                },
+              }}
+              columns={[
+                KEY_RESULT_LIST_COLUMN.KEY_RESULT,
+                KEY_RESULT_LIST_COLUMN.PROGRESS,
+                KEY_RESULT_LIST_COLUMN.TEAM,
+                KEY_RESULT_LIST_COLUMN.OWNER,
+                KEY_RESULT_LIST_COLUMN.OBJECTIVE,
+              ]}
+              bodyProperties={{
+                [KEY_RESULT_LIST_COLUMN.KEY_RESULT]: {
+                  withDynamicIcon: true,
+                  withLastUpdateInfo: true,
+                },
+                [KEY_RESULT_LIST_COLUMN.PROGRESS]: {
+                  withConfidenceTag: true,
+                },
+                [KEY_RESULT_LIST_COLUMN.OWNER]: {
+                  justifyContent: 'flex-end',
+                },
+              }}
+            />
+          </StyledTableWrapper>
         </ModalBody>
       </StyledModal>
     </Modal>
