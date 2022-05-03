751b655c6b608af7f8780cdf312886e26f3cfda3
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-05-03 11:55:20 -0300

    feat(strategy-health/listing): link team tag to team page

--- src/components/KeyResult/List/Body/Columns/Team/team.tsx
+++ src/components/KeyResult/List/Body/Columns/Team/team.tsx
@@ -1,4 +1,5 @@
 import { Tag, TagLabel } from '@chakra-ui/react'
+import Link from 'next/link'
 import React, { ReactElement } from 'react'
 import { useRecoilValue } from 'recoil'
 
@@ -19,14 +20,17 @@ const KeyResultListBodyColumnTeam = ({
   id,
 }: KeyResultListBodyColumnTeamProperties): ReactElement => {
   const team = useRecoilValue(teamSelector(id))
+  const linkTo = team?.id ? `/explore/${team?.id}` : ''
 
   return (
     <KeyResultListBodyColumnBase preventLineClick>
-      <Tag bg="new-gray.200" borderRadius="3px" px="7px" py="5px">
-        <TagLabel color="new-gray.800" fontWeight={500} fontSize="10px" textTransform="uppercase">
-          {team?.name}
-        </TagLabel>
-      </Tag>
+      <Link passHref href={linkTo}>
+        <Tag bg="new-gray.200" borderRadius="3px" px="7px" py="5px">
+          <TagLabel color="new-gray.800" fontWeight={500} fontSize="10px" textTransform="uppercase">
+            {team?.name}
+          </TagLabel>
+        </Tag>
+      </Link>
     </KeyResultListBodyColumnBase>
   )
 }
