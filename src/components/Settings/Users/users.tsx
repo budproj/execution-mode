efbb155e60397391deb9ce6a49f01664a28ee003
diego magalhaes <digengup@gmail.com>
2022-06-13 18:45:12 -0300

    feat(settings-create-user) allow create users on settings page

--- src/components/Settings/Users/users.tsx
+++ src/components/Settings/Users/users.tsx
@@ -1,18 +1,27 @@
 import { Box, Button, Flex, Heading, Stack, Text } from '@chakra-ui/react'
 import React, { useMemo, useState } from 'react'
 import { useIntl } from 'react-intl'
+import { useRecoilValue } from 'recoil'
 
 import { SearchBar } from 'src/components/Base/SearchBar/wrapper'
+import { CreateUserSidebar } from 'src/components/User/Create/Sidebar'
 import UsersTableList from 'src/components/User/TableList'
 import { USERS_TABLE_COLUMN } from 'src/components/User/TableList/Body/Columns/constants'
 import { useGetUsers } from 'src/components/User/hooks/getUsers'
 import { UserStatus } from 'src/components/User/types'
+import meAtom from 'src/state/recoil/user/me'
 
 import messages from './messages'
 
 const SettingsUsers = () => {
   const intl = useIntl()
-  const { data: users, loading } = useGetUsers()
+  const { data: users, loading, refetch } = useGetUsers()
+  const [isCreateSidebarOpen, setIsCreateSidebarOpen] = useState(false)
+
+  const myID = useRecoilValue(meAtom)
+
+  const user = users.find((user) => user.id === myID)
+  const companyId = user?.companies?.edges[0]?.node.id
 
   const [usersFilter, setUsersFilter] = useState('')
   const filteredUsers = users.filter((user) =>
@@ -23,6 +32,16 @@ const SettingsUsers = () => {
     [filteredUsers],
   )
 
+  const onCreate = async () => refetch()
+
+  const handleCreateSidebarOpen = () => {
+    if (!isCreateSidebarOpen) setIsCreateSidebarOpen(true)
+  }
+
+  const handleCreateSidebarClose = () => {
+    if (isCreateSidebarOpen) setIsCreateSidebarOpen(false)
+  }
+
   const columns = [
     USERS_TABLE_COLUMN.NAME,
     USERS_TABLE_COLUMN.TEAMS,
@@ -32,36 +51,45 @@ const SettingsUsers = () => {
   ]
 
   return (
-    <Flex flexDir="column" width="100%">
-      <Heading display="flex" alignItems="center" justifyContent="space-between">
-        <Box flexDir="column">
-          <Text fontSize={24} fontWeight={400} color="#121415" lineHeight="30px" mb={3}>
-            {intl.formatMessage(messages.subTitle)}
-          </Text>
-          <Text fontSize={14} fontWeight={400} color="new-gray.700" lineHeight="17px">
-            {intl.formatMessage(messages.pageDescription, {
-              breakingline: <br />,
-            })}
-          </Text>
-        </Box>
-        <Stack direction="row" gap={2}>
-          <Box w="15rem">
-            <SearchBar
-              placeholder={intl.formatMessage(messages.searchUserInput)}
-              onSearch={setUsersFilter}
-            />
+    <>
+      <Flex flexDir="column" width="100%">
+        <Heading display="flex" alignItems="center" justifyContent="space-between">
+          <Box flexDir="column">
+            <Text fontSize={24} fontWeight={400} color="#121415" lineHeight="30px" mb={3}>
+              {intl.formatMessage(messages.subTitle)}
+            </Text>
+            <Text fontSize={14} fontWeight={400} color="new-gray.700" lineHeight="17px">
+              {intl.formatMessage(messages.pageDescription, {
+                breakingline: <br />,
+              })}
+            </Text>
           </Box>
-          <Button
-            bg="brand.500"
-            color="black.50"
-            _hover={{ background: 'brand.400', color: 'black.50' }}
-          >
-            {intl.formatMessage(messages.createUserButton)}
-          </Button>
-        </Stack>
-      </Heading>
-      <UsersTableList isLoading={loading} usersInfo={usersInfos} pt={10} columns={columns} />
-    </Flex>
+          <Stack direction="row" gap={2}>
+            <Box w="15rem">
+              <SearchBar
+                placeholder={intl.formatMessage(messages.searchUserInput)}
+                onSearch={setUsersFilter}
+              />
+            </Box>
+            <Button
+              bg="brand.500"
+              color="black.50"
+              _hover={{ background: 'brand.400', color: 'black.50' }}
+              onClick={handleCreateSidebarOpen}
+            >
+              {intl.formatMessage(messages.createUserButton)}
+            </Button>
+          </Stack>
+        </Heading>
+        <UsersTableList isLoading={loading} usersInfo={usersInfos} pt={10} columns={columns} />
+      </Flex>
+      <CreateUserSidebar
+        teamID={companyId}
+        isOpen={isCreateSidebarOpen}
+        onClose={handleCreateSidebarClose}
+        onCreate={onCreate}
+      />
+    </>
   )
 }
 
