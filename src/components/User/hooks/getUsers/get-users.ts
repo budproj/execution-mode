e43395d54fbbc5fafe76930efcf5af385fb39175
diego magalhaes <digengup@gmail.com>
2022-06-13 18:43:23 -0300

    refactor(user-listing) update get users hook

--- src/components/User/hooks/getUsers/get-users.ts
+++ src/components/User/hooks/getUsers/get-users.ts
@@ -1,7 +1,8 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, OperationVariables, ApolloQueryResult } from '@apollo/client'
 import { useEffect } from 'react'
 
-import GET_USER_LIST from 'src/components/User/AllReachableUsers/queries.gql'
+import { GetUserPrimaryCompanyQuery } from 'src/components/Report/CompanyProgressOverview/types'
+import GET_USERS from 'src/components/User/hooks/getUsers/get-users.gql'
 import { useConnectionEdges } from 'src/state/hooks/useConnectionEdges/hook'
 import { useRecoilFamilyLoader } from 'src/state/recoil/hooks'
 import { userAtomFamily } from 'src/state/recoil/user'
@@ -12,18 +13,20 @@ interface GetUserListProperties {
   data: User[]
   loading: boolean
   called: boolean
+  refetch: (variables?: Partial<OperationVariables> | undefined) => Promise<ApolloQueryResult<any>>
 }
 
 export const useGetUsers = (): GetUserListProperties => {
   const [loadUsers] = useRecoilFamilyLoader<User>(userAtomFamily)
   const [users, setUsers] = useConnectionEdges<User>()
 
-  const { loading, called } = useQuery(GET_USER_LIST, {
+  const { loading, called, refetch } = useQuery<GetUserPrimaryCompanyQuery>(GET_USERS, {
     fetchPolicy: 'network-only',
     onCompleted: (data) => {
-      const companies = data.users.edges ?? []
+      const companies = data.me?.companies?.edges?.map((edge) => edge.node) ?? []
 
-      setUsers(companies)
+      const users = companies.map((company) => company?.users?.edges ?? []).flat()
+      if (users.length > 0) setUsers(users)
     },
   })
 
@@ -31,5 +34,5 @@ export const useGetUsers = (): GetUserListProperties => {
     loadUsers(users)
   }, [users, loadUsers])
 
-  return { data: users, loading, called }
+  return { data: users, loading, refetch, called }
 }
