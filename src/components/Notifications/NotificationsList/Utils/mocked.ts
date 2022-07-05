1a2a2613ac844f093c948ebd3b36746d55859140
diego magalhaes <digengup@gmail.com>
2022-07-04 21:38:55 -0300

    WIP(notifications) create mocked data

---
+++ src/components/Notifications/NotificationsList/Utils/mocked.ts
@@ -0,0 +1,124 @@
+export const NotificationsMockedArray = [
+  {
+    id: 'af13fb47-b43d-41bf-a8dd-f8eb000d4401',
+    isRead: false,
+    type: 'checkin',
+    timestamp: '2022-06-30T08:55:19.578Z',
+    recipientId: '12312',
+    properties: {
+      sender: {
+        id: '1232',
+        name: 'Ricardo',
+        picture: 'https://cdn.images.express.co.uk/img/dynamic/20/750x445/1278894.jpg',
+      },
+      keyResult: {
+        id: '12331',
+        name: 'Transformar o Bud em uma maquina do tempo!',
+      },
+      previousConfidance: 33,
+      newConfidence: -1,
+    },
+  },
+  {
+    id: 'd21cb2a1-6254-4671-baa9-45ca045bf528',
+    isRead: true,
+    type: 'checkin',
+    timestamp: '2022-06-30T08:55:19.578Z',
+    recipientId: '12312',
+    properties: {
+      sender: {
+        id: '1232',
+        name: 'Ricardo',
+        picture: 'https://cdn.images.express.co.uk/img/dynamic/20/750x445/1278894.jpg',
+      },
+      keyResult: {
+        id: '12331',
+        name: 'Transformar o Bud em uma maquina do tempo!',
+      },
+      previousConfidance: 33,
+      newConfidence: 22,
+    },
+  },
+  {
+    id: '7c7c66de-7f2c-4b63-803c-3b045c375b92',
+    isRead: false,
+    type: 'taskAssign',
+    timestamp: '2022-06-30T08:55:19.578Z',
+    recipientId: '12312',
+    properties: {
+      sender: {
+        id: '1232',
+        name: 'Marcelo',
+        picture:
+          'https://vignette.wikia.nocookie.net/theapartments/images/2/2b/Screen_Shot_2019-02-06_at_11.36.07_AM.png/revision/latest?cb=20190206163327',
+      },
+      task: {
+        id: '12331',
+        name: 'Criar bugs',
+      },
+    },
+  },
+  {
+    id: '8ab23df9-8bf1-42df-a3ed-44f6e258a241',
+    isRead: false,
+    type: 'supportTeam',
+    timestamp: '2022-06-30T08:55:19.578Z',
+    recipientId: '12312',
+    properties: {
+      sender: {
+        id: '1232',
+        name: 'Gui',
+        picture: 'https://i.pinimg.com/originals/72/c3/3b/72c33b5df086100cfcd1c29aa02020b6.png',
+      },
+      keyResult: {
+        id: '12331',
+        name: 'Transformar o Bud em uma maquina do tempo!',
+      },
+    },
+  },
+  {
+    id: '51abab80-5736-4307-9eb3-49633d89e430',
+    isRead: true,
+    type: 'taggedInComment',
+    timestamp: '2022-06-30T08:55:19.578Z',
+    recipientId: '12312',
+    properties: {
+      sender: {
+        id: '1232',
+        name: 'Perin',
+        picture: 'https://static.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png',
+      },
+      keyResult: {
+        id: '12331',
+        name: 'Transformar o Bud em uma maquina do tempo!',
+      },
+      comment: {
+        id: '12331',
+        content:
+          '“Parabéns pela evolução, Rodrigo! Excelente trabalho! Como próximos passos, podemos pensar se faria sentido incluir a área de R&D nas nossas reuniões semanais como uma forma de...”',
+      },
+    },
+  },
+  {
+    id: '839a2830-cb62-4703-bfd3-15fdb10df8fa',
+    isRead: false,
+    type: 'commentOnKR',
+    timestamp: '2022-06-30T08:55:19.578Z',
+    recipientId: '12312',
+    properties: {
+      sender: {
+        id: '1232',
+        name: 'Ricardo',
+        picture: 'https://static.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png',
+      },
+      keyResult: {
+        id: '12331',
+        name: 'Transformar o Bud em uma maquina do tempo!',
+      },
+      comment: {
+        id: '12331',
+        content: 'Se precisar de ajuda é só chamar! =)',
+      },
+    },
+  },
+]
