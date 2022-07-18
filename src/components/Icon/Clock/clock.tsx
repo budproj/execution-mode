0a19119be5c619fbf33a254c70130e682fe5dfa7
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-07-18 14:01:15 -0300

    fix(): remove definition of clip path (#293)

--- src/components/Icon/Clock/clock.tsx
+++ src/components/Icon/Clock/clock.tsx
@@ -11,7 +11,7 @@ const CheckIcon = ({ title, desc, stroke, ...rest }: CheckIconProperties): React
   <Icon viewBox="0 0 11 12" {...rest}>
     <title>{title}</title>
     <desc>{desc}</desc>
-    <g clipPath="url(#clip0_66_1162)">
+    <g>
       <path
         d="M5.50001 10.5614C8.03129 10.5614 10.0833 8.50931 10.0833 5.97803C10.0833 3.44672 8.03129 1.3947 5.50001 1.3947C2.9687 1.3947 0.916672 3.44672 0.916672 5.97803C0.916672 8.50931 2.9687 10.5614 5.50001 10.5614Z"
         stroke={stroke}
@@ -27,16 +27,10 @@ const CheckIcon = ({ title, desc, stroke, ...rest }: CheckIconProperties): React
         strokeLinejoin="round"
       />
     </g>
-    <defs>
-      <clipPath id="clip0_66_1162">
-        <rect width="11" height="11" fill="white" transform="translate(0 0.478027)" />
-      </clipPath>
-    </defs>
   </Icon>
 )
 
 CheckIcon.defaultProps = {
-  fill: 'transparent',
   stroke: '#8491B0',
 }
 
