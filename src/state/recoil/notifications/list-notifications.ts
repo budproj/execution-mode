7f3cc91cc3d11b49e4bf4758b6316e47ed7d0bb8
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-07-27 12:11:43 -0300

    chore(notification): add mock to default state

--- src/state/recoil/notifications/list-notifications.ts
+++ src/state/recoil/notifications/list-notifications.ts
@@ -1,5 +1,6 @@
 import { atom } from 'recoil'
 
+import { NotificationsMockedArray } from 'src/components/Notifications/NotificationsList/Utils/mocked'
 import { Notification } from 'src/components/Notifications/NotificationsList/types'
 
 import { PREFIX } from './constants'
@@ -8,7 +9,7 @@ const KEY = `${PREFIX}::NOTIFICATIONS_ATOM_FAMILY`
 
 export const listNotificationsAtom = atom<Notification[]>({
   key: KEY,
-  default: [],
+  default: NotificationsMockedArray,
 })
 
 export default listNotificationsAtom
