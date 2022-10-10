3b09f0537b1e1e4d6feca3dcf6dc0975105d69d5
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-10-10 14:12:00 -0300

    style(routines): avatar badge for unanswered answers

--- src/components/Routine/RetrospectiveTab/Answers/answer-row.tsx
+++ src/components/Routine/RetrospectiveTab/Answers/answer-row.tsx
@@ -29,7 +29,12 @@ const AnswerRowComponent = ({ answer }: AnswerRowComponentProperties) => {
   return (
     <Flex key={answer.id} marginBottom={5}>
       <Avatar width="45px" height="45px" src={answer.picture} marginRight="15px">
-        <AvatarBadge border="10.5px solid white" boxSize="20px">
+        <AvatarBadge
+          border="2px solid white"
+          boxSize="20px"
+          bgColor="new-gray.200"
+          padding={answer.latestStatusReply ? undefined : '10px'}
+        >
           {answer.latestStatusReply ? (
             getEmoji({ felling: Number(answer.latestStatusReply), size: '20px' })
           ) : (
