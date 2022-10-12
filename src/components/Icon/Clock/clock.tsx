a1f3d7b6738ce9e42bba201e88d3178babdde61c
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-10-12 11:58:26 -0300

    feat(icon/clock): allow icon clock to customize outside stroke

--- src/components/Icon/Clock/clock.tsx
+++ src/components/Icon/Clock/clock.tsx
@@ -5,30 +5,41 @@ import { AcessibleIconProperties } from 'src/components/Icon/types'
 
 export interface CheckIconProperties extends IconProps, AcessibleIconProperties {
   stroke: string
+  outsideStroke?: string
 }
 
-const CheckIcon = ({ title, desc, stroke, ...rest }: CheckIconProperties): ReactElement => (
-  <Icon viewBox="0 0 11 12" {...rest}>
-    <title>{title}</title>
-    <desc>{desc}</desc>
-    <g>
-      <path
-        d="M5.50001 10.5614C8.03129 10.5614 10.0833 8.50931 10.0833 5.97803C10.0833 3.44672 8.03129 1.3947 5.50001 1.3947C2.9687 1.3947 0.916672 3.44672 0.916672 5.97803C0.916672 8.50931 2.9687 10.5614 5.50001 10.5614Z"
-        stroke={stroke}
-        strokeWidth="0.916667"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M5.5 3.22803V5.97803L7.33333 6.89469"
-        stroke={stroke}
-        strokeWidth="0.916667"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </g>
-  </Icon>
-)
+const CheckIcon = ({
+  title,
+  desc,
+  stroke,
+  outsideStroke,
+  ...rest
+}: CheckIconProperties): ReactElement => {
+  const clockCircle = outsideStroke ?? stroke
+
+  return (
+    <Icon viewBox="0 0 11 12" {...rest}>
+      <title>{title}</title>
+      <desc>{desc}</desc>
+      <g>
+        <path
+          d="M5.50001 10.5614C8.03129 10.5614 10.0833 8.50931 10.0833 5.97803C10.0833 3.44672 8.03129 1.3947 5.50001 1.3947C2.9687 1.3947 0.916672 3.44672 0.916672 5.97803C0.916672 8.50931 2.9687 10.5614 5.50001 10.5614Z"
+          stroke={clockCircle}
+          strokeWidth="0.916667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5.5 3.22803V5.97803L7.33333 6.89469"
+          stroke={stroke}
+          strokeWidth="0.916667"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </Icon>
+  )
+}
 
 CheckIcon.defaultProps = {
   stroke: '#8491B0',
