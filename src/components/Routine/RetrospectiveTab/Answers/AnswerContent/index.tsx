877890507411d3ffb2b0a3113a3b1b22002f2de9
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-10-19 14:10:56 -0300

    feat(answer): add user details bar

--- src/components/Routine/RetrospectiveTab/Answers/AnswerContent/index.tsx
+++ src/components/Routine/RetrospectiveTab/Answers/AnswerContent/index.tsx
@@ -1,26 +1,37 @@
-import { Box, VStack } from '@chakra-ui/react'
+import { Box, Divider, VStack } from '@chakra-ui/react'
 import React from 'react'
 import { useRecoilValue } from 'recoil'
 
 import { answerDetailedAtom } from 'src/state/recoil/routine/answer'
+import meAtom from 'src/state/recoil/user/me'
+import selectUser from 'src/state/recoil/user/selector'
 
 import RoutineAnswerCard from './AnswerCards'
 import HistoryAnswers from './AnswerCards/HistoryAnswersCard/history-answers'
+import { UserAnswer } from './AnswerCards/UserAnswer'
 
 const AnswerContent = () => {
   const answerDetailed = useRecoilValue(answerDetailedAtom)
+  const userID = useRecoilValue(meAtom)
+  const user = useRecoilValue(selectUser(userID))
 
   return (
-    <VStack align="flex-start" padding={10}>
-      {answerDetailed.history.length > 0 && (
-        <Box>
-          <HistoryAnswers answers={answerDetailed.history} />
-          {answerDetailed.answers.map((answer) => {
-            return <RoutineAnswerCard key={answer.id} answerData={answer} />
-          })}
-        </Box>
-      )}
-    </VStack>
+    <>
+      {user ? <UserAnswer user={user} /> : undefined}
+
+      <Divider borderColor="new-gray.400" />
+
+      <VStack align="flex-start" padding={10}>
+        {answerDetailed.history.length > 0 && (
+          <Box>
+            <HistoryAnswers answers={answerDetailed.history} />
+            {answerDetailed.answers.map((answer) => {
+              return <RoutineAnswerCard key={answer.id} answerData={answer} />
+            })}
+          </Box>
+        )}
+      </VStack>
+    </>
   )
 }
 
