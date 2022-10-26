30bcd9b31b91c7a50a5814daf88ff40a4d6114a9
Guilherme Luchesi <gui.olhenrique@gmail.com>
2022-10-25 23:17:08 -0300

    refactor(icon): add option to change pause icon background and stroke

--- src/components/Icon/Pause/pause.tsx
+++ src/components/Icon/Pause/pause.tsx
@@ -5,22 +5,31 @@ import { AcessibleIconProperties } from 'src/components/Icon/types'
 
 export interface PauseIconProperties extends IconProps, AcessibleIconProperties {
   viewBox?: string
+  background?: string
+  hasStroke?: boolean
 }
 
-const PauseIcon = ({ title, desc, viewBox, ...rest }: PauseIconProperties): ReactElement => (
+const PauseIcon = ({
+  title,
+  desc,
+  viewBox,
+  background = '#C26EFF',
+  hasStroke = true,
+  ...rest
+}: PauseIconProperties): ReactElement => (
   <Icon viewBox="0 0 22 24" {...rest}>
     <title>{title}</title>
     <desc>{desc}</desc>
-    <circle cx="11" cy="11.9509" r="10" fill="#C26EFF" stroke="white" strokeWidth={2} />
+    <circle
+      cx="11"
+      cy="11.9509"
+      r="10"
+      fill={background}
+      {...(hasStroke ? { stroke: 'white', strokeWidth: 2 } : {})}
+    />
     <rect x="7.88184" y="7.94149" width="1.78195" height="8.01875" rx="0.890973" fill="white" />
     <rect x="12.3364" y="7.94149" width="1.78195" height="8.01875" rx="0.890973" fill="white" />
   </Icon>
 )
-
-PauseIcon.defaultProps = {
-  fill: 'black',
-  stroke: 'black',
-  viewBox: '0 0 8 6',
-}
 
 export default PauseIcon
