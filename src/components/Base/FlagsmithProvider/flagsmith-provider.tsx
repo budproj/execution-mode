301166601ab48a3031079df00d223f55dff66569
Victor Perin <me@victorperin.ninja>
2023-01-02 11:37:18 -0300

    feat(flagsmith-provider): get user traits from business

--- src/components/Base/FlagsmithProvider/flagsmith-provider.tsx
+++ src/components/Base/FlagsmithProvider/flagsmith-provider.tsx
@@ -1,8 +1,11 @@
+import { useQuery } from '@apollo/client'
 import flagsmith from 'flagsmith'
 import { FlagsmithProvider as FlagsmithProviderInternal } from 'flagsmith/react'
 import React, { ReactElement } from 'react'
 
 import getConfig from 'src/config'
+
+import queries from './get-user-traits.gql'
 
 type FlagsmithProviderProperties = {
   children: ReactElement
@@ -10,13 +13,24 @@ type FlagsmithProviderProperties = {
 
 const config = getConfig()
 
-export const FlagsmithProvider = ({ children }: FlagsmithProviderProperties) => (
-  <FlagsmithProviderInternal
-    options={{
-      environmentID: config.publicRuntimeConfig.flagsmith.clientSideKey,
-    }}
-    flagsmith={flagsmith}
-  >
-    {children}
-  </FlagsmithProviderInternal>
-)
+export const FlagsmithProvider = ({ children }: FlagsmithProviderProperties) => {
+  const { data, loading } = useQuery(queries.GET_USER_TRAITS)
+
+  if (!loading) {
+    flagsmith.identify('undefined', {
+      userId: data.me.id,
+      companyId: data.me.companies.edges[0].node.id,
+    })
+  }
+
+  return (
+    <FlagsmithProviderInternal
+      options={{
+        environmentID: config.publicRuntimeConfig.flagsmith.clientSideKey,
+      }}
+      flagsmith={flagsmith}
+    >
+      {loading ? <div /> : children}
+    </FlagsmithProviderInternal>
+  )
+}
