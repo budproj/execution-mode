cbe9e8cd4a742ba1c1b618572e087ee1beb23388
diego magalhaes <digengup@gmail.com>
2023-01-12 18:17:56 -0300

    feat(analytics/team-indicators) create custumizable table base

---
+++ src/components/Base/Table/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useTable, Column } from 'react-table'
+
+interface TablePropertiesM<TData extends Record<string, unknown>> {
+  columns: Array<Column<TData>>
+  data: TData[]
+}
+
+const TableBase = <TData extends Record<string, unknown>>({
+  columns,
+  data,
+}: TablePropertiesM<TData>) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data,
+  })
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()} key={column.id}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()} key={row.id}>
+              {row.cells.map((cell) => {
+                return (
+                  <td {...cell.getCellProps()} key={cell.getCellProps().key}>
+                    {cell.render('Cell')}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+export default TableBase
