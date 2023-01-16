35b58323ff68bb6e126cdfe033f51cf03b02816f
Victor Perin <me@victorperin.ninja>
2023-01-16 17:15:09 -0300

    refactor(lib): remove mirage lib

--- lib/mirage/seeds/selectors.ts
+++
@@ -1,9 +0,0 @@
-import faker from 'faker'
-import { ModelInstance } from 'miragejs'
-
-export const pickRandomModel = <T>(modelInstances: Array<ModelInstance<T>>): T => {
-  const { length } = modelInstances
-  const pickedIndex = faker.random.number(length - 2) + 1
-
-  return modelInstances[pickedIndex]
-}
