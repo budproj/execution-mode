6fc58eed14eede2d1266774601bf8415b4c84ab6
diego magalhaes <digengup@gmail.com>
2023-01-17 17:56:06 -0300

    feat(analytics/team-indicators) create userKeyResultsChecklist column

---
+++ src/components/Team/IndicatorsTable/Columns/key-results-checklists.tsx
@@ -0,0 +1,42 @@
+import { Flex, Square, Text } from '@chakra-ui/react'
+import React from 'react'
+
+import CheckIcon from 'src/components/Icon/Check'
+
+export interface UserChecklistProgressProperties {
+  total: number
+  checked: number
+}
+
+const grayTheme = {
+  bgColor: 'gray.100',
+  color: 'new-gray.800',
+}
+
+const redTheme = {
+  bgColor: 'red.50',
+  color: 'red.500',
+}
+
+const UserChecklistProgress = ({ total, checked }: UserChecklistProgressProperties) => {
+  const colorTheme = total === 0 && checked === 0 ? redTheme : grayTheme
+
+  return (
+    <Flex alignItems="center" justifyContent="center" color={colorTheme.color} gap={2} px={12}>
+      <Square
+        bg={colorTheme.bgColor}
+        color="currentcolor"
+        borderRadius={5}
+        size="1.4em"
+        alignContent="center"
+      >
+        <CheckIcon desc="dsadas" fill="currentcolor" fontWeight="black" w="1.4em" h="1.4em" />
+      </Square>
+      <Text fontSize={16} fontWeight="medium">
+        {total}/{checked}
+      </Text>
+    </Flex>
+  )
+}
+
+export default UserChecklistProgress
