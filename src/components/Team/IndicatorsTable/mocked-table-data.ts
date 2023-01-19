d0114cf124010a18aba77743eeee831ca78998e0
diego magalhaes <digengup@gmail.com>
2023-01-19 13:20:25 -0300

    WIP .

---
+++ src/components/Team/IndicatorsTable/mocked-table-data.ts
@@ -0,0 +1,25 @@
+import { TeamIndicators } from './types'
+
+export const mockedTableData: TeamIndicators[] = [
+  {
+    userKeyResultsOverview: {
+      userId: '922ef72a-6c3c-4075-926a-3245cdeea75f',
+      delta: {
+        confidence: 12,
+        progress: 10,
+      },
+      latestCheckIn: '1970-01-01T00:00:01.900Z',
+      progress: 12,
+    },
+    checklist: {
+      checked: 5,
+      total: 10,
+    },
+    lastAccess: {
+      userId: '922ef72a-6c3c-4075-926a-3245cdeea75f',
+    },
+    lastRetrospectiveAnswer: {
+      userId: '922ef72a-6c3c-4075-926a-3245cdeea75f',
+    },
+  },
+]
--- src/components/Team/IndicatorsTable/wrapper.tsx
+++ src/components/Team/IndicatorsTable/wrapper.tsx
@@ -1,59 +1,53 @@
+/* eslint react/prop-types: 0 */
+
 import React from 'react'
 import { Column } from 'react-table'
 
 import TableBase from 'src/components/Base/Table'
-import UserOverview from 'src/components/User/Overview'
 
-type Movie = {
-  title: string
-  director: string
-  releaseYear: number
-}
-
-type ChecklistProgress = {
-  total: number
-  checked: number
-}
-
-type TeamIndicators = {
-  userID: ChecklistProgress
-  lastAccess: string
-  checklist: string
-  lastRetrospectiveAnswer: string
-}
+import {
+  LastRetrospectiveAnswerColumn,
+  UserKeyResultsChecklistsColumn,
+  UserKeyResultsOverviewColumn,
+  UserLastAccessColumn,
+} from './Columns'
+import { mockedTableData } from './mocked-table-data'
+import { TeamIndicators } from './types'
 
 const IndicatorsTable = () => {
-  const movies: TeamIndicators[] = [
-    {
-      userID: { checked: 1, total: 2 },
-      lastAccess: '213421',
-      checklist: '{ checked: 2, total: 5 }',
-      lastRetrospectiveAnswer: 'dsa',
-    },
-  ]
-
   const columns: Array<Column<TeamIndicators>> = [
     {
       Header: 'Progresso em krs.',
-      accessor: 'userID',
-      // eslint-disable-next-line react/prop-types
-      Cell: ({ cell: { value } }) => <UserOverview userID={value.checked.toString()} />,
+      accessor: 'userKeyResultsOverview',
+      Cell: ({ cell: { value } }) => (
+        <UserKeyResultsOverviewColumn
+          userId={value.userId}
+          progress={value.progress}
+          latestCheckIn={value.latestCheckIn}
+          delta={value.delta}
+        />
+      ),
     },
     {
       Header: 'Último acesso',
       accessor: 'lastAccess',
+      Cell: ({ cell: { value } }) => <UserLastAccessColumn userId={value.userId} />,
     },
     {
       Header: 'Check-list',
       accessor: 'checklist',
+      Cell: ({ cell: { value } }) => (
+        <UserKeyResultsChecklistsColumn total={value.total} checked={value.checked} />
+      ),
     },
     {
       Header: 'Retrospectiva',
       accessor: 'lastRetrospectiveAnswer',
+      Cell: ({ cell: { value } }) => <LastRetrospectiveAnswerColumn userId={value.userId} />,
     },
   ]
 
-  return <TableBase columns={columns} data={movies} />
+  return <TableBase columns={columns} data={mockedTableData} />
 }
 
 export default IndicatorsTable
