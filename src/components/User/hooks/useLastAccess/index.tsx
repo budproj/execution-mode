696cfac67d641eb9cd08a422c03994518017b715
diego magalhaes <digengup@gmail.com>
2023-01-30 13:33:06 -0300

    qa(analytics) update lastAccess text

--- src/components/User/hooks/useLastAccess/index.tsx
+++ src/components/User/hooks/useLastAccess/index.tsx
@@ -1,16 +1,37 @@
+import { useQuery } from '@apollo/client'
 import differenceInDays from 'date-fns/differenceInDays'
 import format from 'date-fns/format'
 import formatDistance from 'date-fns/formatDistance'
+import enUS from 'date-fns/locale/en-US'
 import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 import { useIntl } from 'react-intl'
+
+import queries from 'src/components/User/Create/Form/queries.gql'
 
 import { User } from '../../types'
 import { useGetUserDetails } from '../getUserDetails'
 
 import messages from './messages'
 
+const userLanguage = new Map([
+  ['pt-BR', ptBR],
+  ['en-US', enUS],
+])
+
 export const useGetLastAccess = (userId: User['id']) => {
   const intl = useIntl()
+  const [defaultLocaleID, setDefaultLocaleID] = useState(userLanguage.get('pt-BR'))
+
+  useQuery(queries.GET_USER_LOCALE, {
+    variables: {
+      userID: userId,
+    },
+    onCompleted: (data) => {
+      const locale = data?.user.settings.edges[0]?.node.value
+      if (locale) setDefaultLocaleID(userLanguage.get(locale))
+    },
+  })
 
   const { data: user, loading } = useGetUserDetails(userId)
   const userLastAccessDate = user?.amplitude?.last_used ?? ''
@@ -24,7 +45,7 @@ export const useGetLastAccess = (userId: User['id']) => {
 
       const formatedDistance = formatDistance(new Date(`${userLastAccessDate}T00:00`), new Date(), {
         addSuffix: true,
-        locale: ptBR,
+        locale: defaultLocaleID,
       })
 
       return formatedDistance.charAt(0).toUpperCase() + formatedDistance.slice(1)
