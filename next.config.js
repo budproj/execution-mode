cb8c2c36d2c05c581716499c72140f273c53025a
diego magalhaes <digengup@gmail.com>
2023-02-16 12:45:06 -0300

    feat(next-pwa) disable in localhost

--- next.config.js
+++ next.config.js
@@ -37,7 +37,7 @@ const {
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: HOST === 'localhost',
   skipWaiting: true,
   sw: 'sw.js',
 })
