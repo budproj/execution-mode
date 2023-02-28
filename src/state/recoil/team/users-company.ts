7f326d88a22caba99c3dbddd56cf4b864ff43e33
diego magalhaes <digengup@gmail.com>
2023-02-28 18:28:54 -0300

    refactor(team-summary) create answer summary pagination atom

---
+++ src/state/recoil/team/users-company.ts
@@ -0,0 +1,37 @@
+import { atom, selector } from 'recoil'
+
+import { Team } from 'src/components/Team/types'
+import { User } from 'src/components/User/types'
+
+export const usersCompany = atom<User[]>({
+  key: 'usersCompany',
+  default: [],
+})
+
+export const filteredUsersCompany = (teamId: Team['id']) => {
+  return selector<User[]>({
+    key: 'filteredUsersCompany',
+    get: ({ get }) => {
+      const users = get(usersCompany)
+
+      const filteredUsers = users.filter((user) => {
+        const teamIds = user.teams?.edges.map((team) => team.node.id)
+
+        return teamIds?.includes(teamId)
+      })
+      return filteredUsers
+    },
+  })
+}
+
+export const selectUser = (userId: User['id']) => {
+  return selector<User | undefined>({
+    key: 'selectUser',
+    get: ({ get }) => {
+      const users = get(usersCompany)
+
+      const filteredUsers = users.find(({ id }) => id === userId)
+      return filteredUsers
+    },
+  })
+}
