109f0f6891771416f41eb3ffb72acb2fa92009e7
diego magalhaes <digengup@gmail.com>
2023-03-02 11:30:26 -0300

    refactor(team-summary) update useAnswerSummayPagination hook

--- src/components/Routine/hooks/useAnswerSummaryPagination/use-answer-summary-pagination.tsx
+++ src/components/Routine/hooks/useAnswerSummaryPagination/use-answer-summary-pagination.tsx
@@ -3,10 +3,11 @@ import { useRecoilValue } from 'recoil'
 import { Team } from 'src/components/Team/types'
 import { useGetUserDetails } from 'src/components/User/hooks'
 import { User } from 'src/components/User/types'
+import { answerSummaryAtom } from 'src/state/recoil/routine/answer-summary'
 import { answerSummaryPaginationAtom } from 'src/state/recoil/routine/cursor-answer-summary-pagination'
 import { filteredUsersCompany } from 'src/state/recoil/team/users-company'
 
-const rateLimit = 2
+const rateLimit = 8
 
 type AnswerSummaryPaginationProperties = {
   limitedTeamUsers: User[]
@@ -15,15 +16,17 @@ type AnswerSummaryPaginationProperties = {
 const useAnswerSummaryPagination = (teamId: Team['id']): AnswerSummaryPaginationProperties => {
   const teamUsers = useRecoilValue(filteredUsersCompany(teamId))
   const { lastLoadedUserId } = useRecoilValue(answerSummaryPaginationAtom)
+  const answersSummary = useRecoilValue(answerSummaryAtom)
   const { data: lastLoadedUser } = useGetUserDetails(lastLoadedUserId)
 
   const lastLoadedIndex = teamUsers.findIndex((user) => user.id === lastLoadedUser?.id)
 
-  const cursorPaginationItem = lastLoadedUserId ? lastLoadedIndex : 0
+  const cursorPaginationItem = lastLoadedUserId ? lastLoadedIndex + 1 : 0
 
   const endSlice = cursorPaginationItem + rateLimit
 
-  const limitedTeamUsers = teamUsers.slice(cursorPaginationItem, endSlice)
+  const limitedTeamUsers =
+    answersSummary.length < teamUsers.length ? teamUsers.slice(cursorPaginationItem, endSlice) : []
 
   return { limitedTeamUsers }
 }
