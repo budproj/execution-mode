3ce467228367c7ec30f272806ca30403a2ba7e3a
Rodrigo Antas <rodr.antas@gmail.com>
2023-03-21 17:13:35 -0300

    fix(title): fixing ranking title

--- src/components/Report/TeamsOverview/messages.ts
+++ src/components/Report/TeamsOverview/messages.ts
@@ -7,17 +7,17 @@ type TeamOverviewMessages =
 
 export default defineMessages<TeamOverviewMessages>({
   teamRankingTitle: {
-    defaultMessage: 'Ranking de Times do {quarter}',
+    defaultMessage: 'Ranking de Times',
     id: 'ucALDM',
     description: 'Title to show on the team ranking page',
   },
   teamRankingTitleWithoutGamification: {
-    defaultMessage: 'Progresso dos Times no {quarter}',
+    defaultMessage: 'Progresso dos Times',
     id: 'dH7eW3',
     description: 'Title to show on the team ranking page when gamification is disabled',
   },
   teamRankingSubTitle: {
-    defaultMessage: 'Progresso geral dos objetivos no ciclo trimestral atual',
+    defaultMessage: 'Progresso geral dos objetivos no ciclo atual',
     id: 'BROY+x',
     description: 'Subtitle to show on the team ranking page when gamification is disabled',
   },
