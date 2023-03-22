6ccacb9a95b41238f446438faa5c759a1d7accf4
diego magalhaes <digengup@gmail.com>
2023-03-22 16:44:57 -0300

    refactor(team-highlights) fix team column styles

--- src/components/Team/RoutineHighlightsTable/Columns/User/index.tsx
+++ src/components/Team/RoutineHighlightsTable/Columns/User/index.tsx
@@ -1,9 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 import Link from 'next/link'
 import React from 'react'
 
 import { useGetUserDetails } from 'src/components/User/hooks'
 import { User } from 'src/components/User/types'
+
+export const CustomFlex = styled(Flex)`
+  @media (min-width: 1600px) {
+    max-width: 335px;
+  }
+
+  @media (max-width: 1417px) {
+    max-width: 260px;
+  }
+`
 
 export interface RoutinesHighlightsTableUserColumnProperties {
   userId: User['id']
@@ -16,7 +27,7 @@ const RoutinesHighlightsTableUserColumn = ({
 
   return (
     <Link passHref href={`/profile/${userId}`}>
-      <Flex color="new-gray.800" fontWeight="500" fontSize="12px">
+      <CustomFlex color="new-gray.800" fontWeight="500" fontSize="12px">
         <Avatar src={user?.picture} width="50px" height="50px" marginRight="20px" />
         <Box>
           <Text color="new-gray.900" fontWeight={400} fontSize="16px">
@@ -26,7 +37,7 @@ const RoutinesHighlightsTableUserColumn = ({
             {user?.role}
           </Text>
         </Box>
-      </Flex>
+      </CustomFlex>
     </Link>
   )
 }
