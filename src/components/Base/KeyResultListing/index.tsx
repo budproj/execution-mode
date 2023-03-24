007e320ed0b14625d7e2d97817e87946334cfa48
diego magalhaes <digengup@gmail.com>
2023-03-24 15:54:35 -0300

    feat(pagination-test) implementing pagination on KRs list table

--- src/components/Base/KeyResultListing/index.tsx
+++ src/components/Base/KeyResultListing/index.tsx
@@ -7,16 +7,22 @@ import {
   Heading,
   Flex,
   Button,
+  HStack,
 } from '@chakra-ui/react'
 import styled from '@emotion/styled'
-import React, { useEffect, useMemo } from 'react'
-import { useSetRecoilState } from 'recoil'
+import React, { useEffect, useMemo, useState } from 'react'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 
 import KeyResultList from 'src/components/KeyResult/List'
 import { KEY_RESULT_LIST_COLUMN } from 'src/components/KeyResult/List/Body/Columns/constants'
 import { KEY_RESULT_LIST_TYPE } from 'src/components/KeyResult/List/constants'
+import {
+  FetchMoreVariables,
+  KRS_PER_PAGE,
+} from 'src/components/KeyResult/hooks/getKeyResults/get-key-results'
 import { KeyResult } from 'src/components/KeyResult/types'
 import { keyResultReadDrawerOpenedKeyResultID } from 'src/state/recoil/key-result/drawers/read/opened-key-result-id'
+import { krTableLengthAtom } from 'src/state/recoil/key-result/kr-table-lenght.atom'
 
 export interface KeyResultModalListingProperties {
   isOpen: boolean
@@ -25,7 +31,7 @@ export interface KeyResultModalListingProperties {
   data: KeyResult[]
   dispatchEvent?: () => void
   modalHeadingTitle: string
-  fetchMore: any
+  fetchMore?: ({ limit, offset }: FetchMoreVariables) => Promise<void>
 }
 
 const StyledModal = styled(ModalContent)`
@@ -60,8 +66,60 @@ export const KeyResultListingModal = ({
   fetchMore,
 }: KeyResultModalListingProperties) => {
   const setOpenDrawer = useSetRecoilState(keyResultReadDrawerOpenedKeyResultID)
+  const krTableLength = useRecoilValue(krTableLengthAtom)
 
-  const keyResultIds = useMemo(() => data.map(({ id }) => id), [data])
+  const [lastKrListed, setLastKrListed] = useState({
+    firstListElement: 0,
+    lastListElement: KRS_PER_PAGE,
+  })
+  const dataToRender = useMemo(() => {
+    return data.slice(lastKrListed.firstListElement, lastKrListed.lastListElement)
+  }, [data, lastKrListed])
+
+  const keyResultIds = useMemo(() => dataToRender.map(({ id }) => id), [dataToRender])
+
+  const firstListKeyResultIndex = data.findIndex((kr) => kr.id === keyResultIds[0])
+
+  const showPreviousPageButton =
+    !loadingData && firstListKeyResultIndex > 0 && keyResultIds.length < data.length
+
+  const showNextPageButton =
+    !loadingData &&
+    krTableLength > 0 &&
+    (krTableLength > data.length ||
+      (data.length === krTableLength && krTableLength > lastKrListed.lastListElement))
+
+  const loadNextKrsPage = async () => {
+    if (fetchMore) {
+      const lastRenderedIndex = data.findIndex(
+        (kr) => kr.id === keyResultIds[keyResultIds.length - 1],
+      )
+
+      const lastDataIndex = data.indexOf(data[data.length - 1])
+      const mustFetchMore = lastRenderedIndex === lastDataIndex
+
+      if (mustFetchMore) {
+        await fetchMore({
+          limit: KRS_PER_PAGE,
+          offset: data.length,
+        })
+      }
+
+      setLastKrListed({
+        firstListElement: lastRenderedIndex + 1,
+        lastListElement: lastRenderedIndex + KRS_PER_PAGE + 1,
+      })
+    }
+  }
+
+  const loadPreviousKrsPage = () => {
+    const firstRenderedIndex = data.findIndex((kr) => kr.id === keyResultIds[0])
+
+    setLastKrListed({
+      firstListElement: firstRenderedIndex - KRS_PER_PAGE,
+      lastListElement: firstRenderedIndex,
+    })
+  }
 
   useEffect(() => {
     if (dispatchEvent) dispatchEvent()
@@ -94,17 +152,6 @@ export const KeyResultListingModal = ({
           </Flex>
 
           <StyledTableWrapper>
-            <Button
-              onClick={() =>
-                fetchMore({
-                  variables: {
-                    offset: 2,
-                  },
-                })
-              }
-            >
-              aaa
-            </Button>
             <KeyResultList
               type={KEY_RESULT_LIST_TYPE.STATIC}
               keyResultIDs={keyResultIds}
@@ -148,6 +195,28 @@ export const KeyResultListingModal = ({
               onLineClick={onLineClick}
             />
           </StyledTableWrapper>
+          <HStack width="100%" mt={10} alignItems="center" justifyContent="flex-end">
+            {showPreviousPageButton && (
+              <Button
+                bg="brand.500"
+                color="white"
+                _hover={{ backgroundColor: 'brand.300' }}
+                onClick={loadPreviousKrsPage}
+              >
+                Prev
+              </Button>
+            )}
+            {showNextPageButton && (
+              <Button
+                bg="brand.500"
+                color="white"
+                _hover={{ backgroundColor: 'brand.300' }}
+                onClick={loadNextKrsPage}
+              >
+                Next
+              </Button>
+            )}
+          </HStack>
         </ModalBody>
       </StyledModal>
     </Modal>
