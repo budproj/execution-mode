588f2f61ffb1451d627ca82a8b28ce55320b8053
diego magalhaes <digengup@gmail.com>
2023-03-31 13:53:44 -0300

    Revert "feat(users-pagination) set loading state to false when the search input is empty"
    This reverts commit 9dd653a7ce7dd09af28f495c3b3b5dc3561fb72b.

--- src/components/Routine/RetrospectiveTab/Answers/index.tsx
+++ src/components/Routine/RetrospectiveTab/Answers/index.tsx
@@ -165,7 +165,6 @@ const AnswersComponent = memo(({ teamId, after, before, week }: AnswersComponent
         setIsAnswerSummaryLoading(true)
         await debouncedSearch(value)
       } else {
-        setIsAnswerSummaryLoading(false)
         setSearch('')
       }
     },
