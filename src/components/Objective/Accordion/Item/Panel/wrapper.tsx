c9626e8501cb951f0f241f2cbc002f3958d784dc
diego magalhaes <digengup@gmail.com>
2023-05-22 09:07:31 -0300

    WIO(key-results-lifecycle) publish okr workflow

--- src/components/Objective/Accordion/Item/Panel/wrapper.tsx
+++ src/components/Objective/Accordion/Item/Panel/wrapper.tsx
@@ -1,6 +1,7 @@
-import { AccordionPanel, Text } from '@chakra-ui/react'
-import React from 'react'
+import { AccordionPanel, Button, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
+import ConfirmPublishingDialog from 'src/components/Objective/OKRsPublishingFlow/ConfirmPublishingDialog/confirm-publishing-dialog'
 import { Objective } from 'src/components/Objective/types'
 import { Team } from 'src/components/Team/types'
 import { ObjectiveContext, ObjectiveViewMode } from 'src/state/recoil/objective/context'
@@ -27,26 +28,31 @@ export const ObjectiveAccordionPanel = ({
   context,
 }: ObjectiveAccordionPanelProperties) => {
   const isEditing = context?.mode === ObjectiveViewMode.EDIT
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
 
   return (
-    <AccordionPanel p={0} maxWidth="100%">
-      {isExpanded && (
-        <>
-          {description && !isEditing && (
-            <Text fontSize="14px" fontWeight={400} color="new-gray.700" pt={6} pb={3} pl={2}>
-              {description}
-            </Text>
-          )}
-          <ObjectiveKeyResults
-            isDraft={isDraft}
-            objectiveID={objectiveID}
-            mode={context?.mode}
-            context={context}
-            isDisabled={isDisabled}
-            teamID={teamID}
-          />
-        </>
-      )}
-    </AccordionPanel>
+    <>
+      <AccordionPanel p={0} maxWidth="100%">
+        {isExpanded && (
+          <>
+            {description && !isEditing && (
+              <Text fontSize="14px" fontWeight={400} color="new-gray.700" pt={6} pb={3} pl={2}>
+                {description}
+              </Text>
+            )}
+            <ObjectiveKeyResults
+              isDraft={isDraft}
+              objectiveID={objectiveID}
+              mode={context?.mode}
+              context={context}
+              isDisabled={isDisabled}
+              teamID={teamID}
+            />
+            <Button onClick={() => setIsDialogOpen(true)}>Publicar OKR</Button>
+          </>
+        )}
+      </AccordionPanel>
+      <ConfirmPublishingDialog isOpen={isDialogOpen} onClose={() => setIsDialogOpen(false)} />
+    </>
   )
 }
