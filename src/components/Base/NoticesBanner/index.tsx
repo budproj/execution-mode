9f7f98004f0182b9e57ad48ecd5d879c29edc72a
Diêgo de Lima Magalhães <digengup@gmail.com>
2023-06-30 11:55:30 -0300

    Refactor/maintenance notice (#402)
    * refactor(maintenance_notice) create maintanance notice banner
    
    * refactor(maintenance_notice) update banner bg color

--- src/components/Base/NoticesBanner/index.tsx
+++ src/components/Base/NoticesBanner/index.tsx
@@ -1,30 +1,16 @@
-import { Box, Button, CloseButton, Collapse, Flex, Text } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
+import { Box, CloseButton, Collapse, Flex, Text } from '@chakra-ui/react'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useIntl } from 'react-intl'
-import { useRecoilValue } from 'recoil'
 
-import { useRoutineTab } from 'src/components/Routine/hooks/getRoutineTab'
-import { EventType } from 'src/state/hooks/useEvent/event-type'
-import { useEvent } from 'src/state/hooks/useEvent/hook'
 import useLocalStorage from 'src/state/hooks/useLocalStorage/hook'
-import meAtom from 'src/state/recoil/user/me'
-import selectUser from 'src/state/recoil/user/selector'
 
 import messages from './messages'
 
-const storageKey = 'Bud@new-feature'
+const storageKey = 'Bud@notices'
 
 const NoticesBanner = () => {
   const { get, register } = useLocalStorage()
   const intl = useIntl()
-  const { dispatch: learnMoreClick } = useEvent(EventType.LEARN_MORE_BANNER_NOTICES_CLICK)
-
-  const routineTabName = useRoutineTab()
-  const router = useRouter()
-
-  const userID = useRecoilValue(meAtom)
-  const user = useRecoilValue(selectUser(userID))
 
   const [isOpen, setIsOpen] = useState(false)
 
@@ -43,11 +29,6 @@ const NoticesBanner = () => {
     return () => clearTimeout(time)
   }, [get])
 
-  const handleRedirect = useCallback(() => {
-    learnMoreClick({})
-    router.push(`/explore/${user?.companies?.edges[0]?.node.id ?? ''}?activeTab=${routineTabName}`)
-  }, [learnMoreClick, router, routineTabName, user?.companies?.edges])
-
   const handleCloseBanner = useCallback(() => {
     register(storageKey, false)
     setIsOpen(false)
@@ -61,29 +42,20 @@ const NoticesBanner = () => {
         display="flex"
         alignItems="center"
         justifyContent="center"
-        bg="linear-gradient(to right, #F53D7A, #1E97F7)"
+        bg="linear-gradient(to right,  #BF4950, #FF616A, #FF8188)"
       >
         <Flex gap={6} alignItems="center">
-          <Text color="brand.50" fontSize={14} fontWeight="medium">
-            {intl.formatMessage(messages.learMoreAboutRetrospectiveMessageBanner)}
+          <Text color="white" fontSize={14} fontWeight="medium">
+            {intl.formatMessage(messages.maintenanceNoticeMessageBanner)}
           </Text>
-          <Button
-            p="12px 22px 12px 22px"
-            height="100%"
-            borderRadius={4}
-            border="1px solid"
-            color="brand.50"
-            fontSize={12}
-            lineHeight={0}
-            _hover={{
-              color: 'brand.200',
-            }}
-            onClick={handleRedirect}
-          >
-            {intl.formatMessage(messages.learMoreActionButton)}
-          </Button>
         </Flex>
-        <CloseButton position="absolute" right={4} color="brand.50" onClick={handleCloseBanner} />
+        <CloseButton
+          position="absolute"
+          right={4}
+          fontSize={14}
+          color="white"
+          onClick={handleCloseBanner}
+        />
       </Box>
     </Collapse>
   )
--- src/components/Base/NoticesBanner/messages.ts
+++ src/components/Base/NoticesBanner/messages.ts
@@ -1,18 +1,13 @@
 import { defineMessages } from 'react-intl'
 
-type NoticesBannerMessages = 'learMoreAboutRetrospectiveMessageBanner' | 'learMoreActionButton'
+type NoticesBannerMessages = 'maintenanceNoticeMessageBanner'
 
 export default defineMessages<NoticesBannerMessages>({
-  learMoreAboutRetrospectiveMessageBanner: {
-    defaultMessage: 'Como foi sua semana? Coloque sua Retrospectiva em dia',
-    id: 'Yjqtno',
+  maintenanceNoticeMessageBanner: {
+    defaultMessage:
+      'AVISO DE MANUTENÇÃO: A plataforma estará fora de operação do dia 01 ao dia 2 de Julho para manutenção',
+    id: 'Eq4/e4',
     description:
       'This message appears on the news banner to invite the user to learn about the Retrospective feature.',
-  },
-  learMoreActionButton: {
-    defaultMessage: 'Saiba mais',
-    id: 'uveo/4',
-    description:
-      'This message appears on action button that redirect the user to company page on Retrospective tab',
   },
 })
