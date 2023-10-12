4cd181b26c408fdbfd0e42e08ffcc290b116b14e
Rodrigo Antas <rodr.antas@gmail.com>
2023-10-12 10:19:43 -0300

    fix(task): fixing done task text

---
+++ src/components/Base/TasksCard/messages.ts
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl'
+
+type TaskCardMessages = 'doneTask'
+
+export default defineMessages<TaskCardMessages>({
+  doneTask: {
+    defaultMessage: 'Tarefa feita',
+    id: 'uM5lpV',
+    description: 'The text that appears when the task is done.',
+  },
+})
