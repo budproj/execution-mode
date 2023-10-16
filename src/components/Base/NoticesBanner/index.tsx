394e62dbf72a7c38df5c67d75c91419f5a021147
Rodrigo Antas <rodr.antas@gmail.com>
2023-10-16 12:16:01 -0300

    feat(notices-banner): adding nps from october 2023

--- src/components/Base/NoticesBanner/index.tsx
+++ src/components/Base/NoticesBanner/index.tsx
@@ -1,19 +1,24 @@
 import { Box, Button, CloseButton, Collapse, Flex, Text } from '@chakra-ui/react'
 import React, { useCallback, useEffect, useState } from 'react'
+import { useIntl } from 'react-intl'
 
 import useLocalStorage from 'src/state/hooks/useLocalStorage/hook'
 
-export const storageKey = ''
+import messages from './messages'
+
+export const storageKey = 'Bud@NPSOctober'
 
 const NoticesBanner = () => {
   const { get, register } = useLocalStorage()
 
-  const [_, setIsOpen] = useState(false)
+  const intl = useIntl()
+
+  const [isOpen, setIsOpen] = useState(false)
 
   useEffect(() => {
     const time = setTimeout(() => {
       setIsOpen(() => {
-        const valueStoraged = get('')
+        const valueStoraged = get(storageKey)
         if (valueStoraged === false) {
           return valueStoraged
         }
@@ -26,12 +31,12 @@ const NoticesBanner = () => {
   }, [get])
 
   const handleCloseBanner = useCallback(() => {
-    register('', false)
+    register(storageKey, false)
     setIsOpen(false)
   }, [register])
 
   return (
-    <Collapse animateOpacity in={false}>
+    <Collapse animateOpacity in={isOpen}>
       <Box
         width="100%"
         h={54}
@@ -41,8 +46,15 @@ const NoticesBanner = () => {
         bg="linear-gradient(to right, #F53D7A, #1E97F7)"
       >
         <Flex gap={6} alignItems="center">
-          <Text color="brand.50" fontSize={14} fontWeight="medium" />
-          <a href="/" target="_blank" style={{ textDecoration: 'none' }} rel="noreferrer">
+          <Text color="brand.50" fontSize={14} fontWeight="medium">
+            {intl.formatMessage(messages.mainText)}
+          </Text>
+          <a
+            href="https://bit.ly/npsprodutobud"
+            target="_blank"
+            style={{ textDecoration: 'none' }}
+            rel="noreferrer"
+          >
             <Button
               p="12px 22px 12px 22px"
               height="100%"
@@ -54,7 +66,9 @@ const NoticesBanner = () => {
               _hover={{
                 color: 'brand.200',
               }}
-            />
+            >
+              {intl.formatMessage(messages.buttonText)}
+            </Button>
           </a>
         </Flex>
         <CloseButton position="absolute" right={4} color="brand.50" onClick={handleCloseBanner} />
