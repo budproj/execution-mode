1cede4c4acc2a7315cee96b6d9400308a5c6148f
Rodrigo Antas <rodr.antas@gmail.com>
2024-03-02 01:18:33 -0300

    removing comment array change

--- src/components/TaskManagement/TaskDrawer/Timeline/timeline-wrapper.tsx
+++ src/components/TaskManagement/TaskDrawer/Timeline/timeline-wrapper.tsx
@@ -18,8 +18,6 @@ const getKey = (item: any) => {
 }
 
 export const TimelineWrapper = ({ comments, updates }: TasProperties) => {
-  comments = []
-  console.log({ updates })
   const timeline = orderBy([...comments, ...updates], 'createdAt', 'desc')
 
   return (
