756265f4cd2d4668bb3a27c549b60839d03906c4
Rodrigo Antas <rodr.antas@gmail.com>
2024-03-11 12:10:47 -0300

    fix(delete-comment): fixing variable name

--- src/components/Routine/hooks/deleteComment/delete-comment.ts
+++ src/components/Routine/hooks/deleteComment/delete-comment.ts
@@ -12,14 +12,12 @@ type useGetCommentsByEntityProperties = {
 
 export const useDeleteComment = () => {
   const { servicesPromise } = useContext(ServicesContext)
-  const setRoutineComment = useSetRecoilState(commentsAtom)
+  const setComments = useSetRecoilState(commentsAtom)
 
   const deleteComment = async ({ id }: useGetCommentsByEntityProperties) => {
     const { comments: commentsService } = await servicesPromise
     await commentsService.delete<Comment[]>(`/comments/${id}`)
-    setRoutineComment((previousComments) =>
-      [...previousComments].filter((comment) => comment.id !== id),
-    )
+    setComments((previousComments) => [...previousComments].filter((comment) => comment.id !== id))
   }
 
   return { deleteComment }
