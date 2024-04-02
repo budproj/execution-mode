d91e2f3a1d4b372ab02a98c4473245f133ac0881
Rodrigo Antas <rodr.antas@gmail.com>
2024-04-02 10:25:25 -0300

    removing unused const

--- src/components/Base/MainAppBar/TeamHoverMenu/team-hover-menu.tsx
+++ src/components/Base/MainAppBar/TeamHoverMenu/team-hover-menu.tsx
@@ -10,10 +10,8 @@ import { GraphQLConnection } from 'src/components/types'
 import { useConnectionEdges } from 'src/state/hooks/useConnectionEdges/hook'
 import { EventType } from 'src/state/hooks/useEvent/event-type'
 import { useEvent } from 'src/state/hooks/useEvent/hook'
-import useLocalStorage from 'src/state/hooks/useLocalStorage/hook'
 
 import IntlLink from '../../IntlLink'
-import { storageKey } from '../../NoticesBanner'
 
 import messages from './messages'
 import queries from './queries.gql'
@@ -41,9 +39,9 @@ const TeamHoverMenu = ({
   const { data } = useQuery<GetOtherTeamsQuery>(queries.GET_OTHER_TEAMS)
   const [teams, setEdges] = useConnectionEdges<Team>()
 
-  const { get } = useLocalStorage()
+  // Const { get } = useLocalStorage()
 
-  const valueStoraged = get(storageKey) || get(storageKey) === false
+  // Const valueStoraged = get(storageKey) || get(storageKey) === false
 
   useEffect(() => {
     if (data) {
