2f1279458b2f581945c53ff0b2251c981430b0da
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2024-12-16 10:23:07 -0300

    fix:Temporary removed chart progress until migration

--- src/components/KeyResult/Single/Sections/Progress/wrapper.tsx
+++ src/components/KeyResult/Single/Sections/Progress/wrapper.tsx
@@ -1,17 +1,19 @@
-import { Collapse, IconButton, Skeleton, Stack } from '@chakra-ui/react'
-import React, { useState } from 'react'
+//import { Collapse, IconButton, Skeleton, Stack } from '@chakra-ui/react'
+import {Skeleton, Stack } from '@chakra-ui/react'
+//import React, { useState } from 'react'
+import React from 'react'
 import { useIntl } from 'react-intl'
 import { useRecoilValue } from 'recoil'
 
-import ChevronDownIcon from 'src/components/Icon/ChevronDown'
+//import ChevronDownIcon from 'src/components/Icon/ChevronDown'
 import { PercentageNumberMask } from 'src/components/KeyResult/NumberMasks'
 import ProgressSlider from 'src/components/KeyResult/ProgressSlider/wrapper'
-import { EventType } from 'src/state/hooks/useEvent/event-type'
-import { useEvent } from 'src/state/hooks/useEvent/hook'
+//import { EventType } from 'src/state/hooks/useEvent/event-type'
+//import { useEvent } from 'src/state/hooks/useEvent/hook'
 import { keyResultAtomFamily } from 'src/state/recoil/key-result'
 
 import { KeyResultSectionHeading } from '../Heading/wrapper'
-import { ProgressHistoryChart } from '../ProgressHistory/wrapper'
+//import { ProgressHistoryChart } from '../ProgressHistory/wrapper'
 
 import messages from './messages'
 
@@ -21,19 +23,19 @@ type KeyResultProgressProperties = {
 
 export const KeyResultProgress = ({ keyResultID }: KeyResultProgressProperties) => {
   const intl = useIntl()
-  const [isGraphOpen, setIsGraphOpen] = useState(false)
+  //const [isGraphOpen, setIsGraphOpen] = useState(false)
   const keyResult = useRecoilValue(keyResultAtomFamily(keyResultID))
-  const { dispatch } = useEvent(EventType.KEY_RESULT_PROGRESS_CHART_VIEW)
+  //const { dispatch } = useEvent(EventType.KEY_RESULT_PROGRESS_CHART_VIEW)
 
   const isLoaded = Boolean(keyResult)
   const isActive = keyResult?.status?.isActive
-
+  /*
   const handleGraphToggle = () => {
     setIsGraphOpen(!isGraphOpen)
 
     if (!isGraphOpen && keyResultID) dispatch({ keyResultID })
   }
-
+  */
   return (
     <>
       <Stack spacing={4} direction="row" alignItems="center">
@@ -50,7 +52,7 @@ export const KeyResultProgress = ({ keyResultID }: KeyResultProgressProperties) 
             decimalScale={0}
           />
         </Skeleton>
-
+        {/*
         <IconButton
           color="new-gray.900"
           minW="auto"
@@ -65,11 +67,14 @@ export const KeyResultProgress = ({ keyResultID }: KeyResultProgressProperties) 
             />
           }
           onClick={handleGraphToggle}
-        />
+        
+        />*/}
       </Stack>
+      {/*
       <Collapse in={isGraphOpen}>
         <ProgressHistoryChart keyResultID={keyResultID} />
       </Collapse>
+      */}
     </>
   )
 }
