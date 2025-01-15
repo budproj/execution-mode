3fd5c976b3963ee82f731fa9a62a35f2ab1a7bc4
Rodrigo Seiji Piubeli Hirao <ctw03167@criticaltechworks.com>
2025-01-15 13:43:29 -0300

    Feat: NPS form.

--- src/components/Base/NoticesBanner/index.tsx
+++ src/components/Base/NoticesBanner/index.tsx
@@ -1,8 +1,11 @@
 import { Box, Button, CloseButton, Collapse, Flex, Text } from '@chakra-ui/react'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useIntl } from 'react-intl'
+import { useRecoilValue } from 'recoil'
 
 import useLocalStorage from 'src/state/hooks/useLocalStorage/hook'
+import meAtom from 'src/state/recoil/user/me'
+import selectUser from 'src/state/recoil/user/selector'
 
 import messages from './messages'
 
@@ -10,25 +13,29 @@ export const storageKey = 'bud-platform-speed-survey'
 
 const NoticesBanner = () => {
   const { get, register } = useLocalStorage()
+  const userID = useRecoilValue(meAtom)
+  const user = useRecoilValue(selectUser(userID))
 
   const intl = useIntl()
 
   const [isOpen, setIsOpen] = useState(false)
+  const [formUrl, setFormUrl] = useState('')
 
   useEffect(() => {
     const time = setTimeout(() => {
-      setIsOpen(() => {
-        const valueStoraged = get(storageKey)
-        if (valueStoraged === '10-12-24') {
-          return false
-        }
-
-        return true
-      })
+      setIsOpen(() => get(storageKey) !== '15-01-25')
     }, 2300)
 
     return () => clearTimeout(time)
   }, [get])
+
+  useEffect(() => {
+    if (user?.companies) {
+      setFormUrl(
+        `https://asm6am1qamy.typeform.com/to/ws7KOlJ6?utm_term=${user.companies.edges[0].node?.name}&utm_source=getbud&utm_medium=app&utm_campaign=nps25q1`,
+      )
+    }
+  }, [user])
 
   const handleCloseBanner = useCallback(() => {
     register(storageKey, '10-12-24')
@@ -36,7 +43,7 @@ const NoticesBanner = () => {
   }, [register])
 
   return (
-    <Collapse animateOpacity in={isOpen}>
+    <Collapse animateOpacity in={isOpen && user?.companies !== undefined}>
       <Box
         width="100%"
         h={54}
@@ -49,12 +56,7 @@ const NoticesBanner = () => {
           <Text color="#fff" fontSize={14} fontWeight="medium">
             {intl.formatMessage(messages.mainText)}
           </Text>
-          <a
-            href="https://bit.ly/csatvelocidade"
-            target="_blank"
-            style={{ textDecoration: 'none' }}
-            rel="noreferrer"
-          >
+          <a href={formUrl} target="_blank" style={{ textDecoration: 'none' }} rel="noreferrer">
             <Button
               p="12px 22px 12px 22px"
               height="100%"
