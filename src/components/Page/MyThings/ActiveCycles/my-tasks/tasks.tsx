0c64dc723fdaf054c69dc3096ee5a813da60aad8
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-02-19 15:14:45 -0300

    fix: new task manager options

--- src/components/Page/MyThings/ActiveCycles/my-tasks/tasks.tsx
+++ src/components/Page/MyThings/ActiveCycles/my-tasks/tasks.tsx
@@ -1,13 +1,15 @@
 import { Stack, Text, Box, HStack } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { useIntl } from 'react-intl'
 
 import { Accordion } from 'src/components/Base/Accordion'
 import { KeyResultDynamicIcon } from 'src/components/KeyResult'
 import { KeyResultChecklist } from 'src/components/KeyResult/Single/Sections/Checklist/checklist'
-import { KeyResult, KeyResultCheckMark } from 'src/components/KeyResult/types'
-import { GraphQLEffect } from 'src/components/types'
-import { useConnectionEdges } from 'src/state/hooks/useConnectionEdges/hook'
+import { KeyResult } from 'src/components/KeyResult/types'
+import { useGetNewTask } from 'src/components/TaskManagement/hooks/use-get-tasks-new'
+//import { GraphQLEffect } from 'src/components/types'
+//import { useConnectionEdges } from 'src/state/hooks/useConnectionEdges/hook'
 
 import messages from './messages'
 
@@ -23,12 +25,30 @@ interface KeyResultTasksProperties {
 }
 
 const KeyResultTasks = ({ keyResult, createTaskLabel, onUpdate }: KeyResultTasksProperties) => {
-  const [checklist, updateChecklistEdges] = useConnectionEdges<KeyResultCheckMark>()
+  /*onst [checklist, updateChecklistEdges] = useConnectionEdges<KeyResultCheckMark>()
   const canCreate = keyResult.checkList?.policy?.create === GraphQLEffect.ALLOW
 
   useEffect(() => {
     updateChecklistEdges(keyResult.checkList?.edges)
-  }, [keyResult.checkList?.edges, updateChecklistEdges])
+  }, [keyResult.checkList?.edges, updateChecklistEdges])*/
+
+  const router = useRouter()
+  const { id } = router.query
+
+
+  const {
+    data: tasks = [],
+    refetch,
+  } = useGetNewTask(id as string, keyResult.id ?? '')
+
+  const hasItems = tasks.length > 0
+
+  const canCreate = !hasItems
+  
+
+  useEffect(() => {
+    if (keyResult.id) refetch()
+  }, [keyResult.id, refetch])
 
   return (
     <Accordion
@@ -47,7 +67,7 @@ const KeyResultTasks = ({ keyResult, createTaskLabel, onUpdate }: KeyResultTasks
           isEditable
           checkPolicy={false}
           keyResultID={keyResult.id}
-          nodes={checklist}
+          nodes={tasks}
           canCreate={canCreate}
           wrapperProps={{ pt: 0 }}
           createTaskLabel={createTaskLabel}
