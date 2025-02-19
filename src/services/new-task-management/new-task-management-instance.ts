2599e86d99672a798caf336471bf8bfda987198b
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-02-19 15:38:02 -0300

    fix: prepare baseUrl to receive new backend route

--- src/services/new-task-management/new-task-management-instance.ts
+++ src/services/new-task-management/new-task-management-instance.ts
@@ -1,9 +1,16 @@
 import axios from 'axios'
 
-import { BudConfig } from 'src/config'
+//import { BudConfig } from 'src/config'
 
-export const getNewTaskManagementInstance = (config: BudConfig) =>
+export const getNewTaskManagementInstance = () =>
   axios.create({
     baseURL:
      "http://127.0.0.1:8000/"
   })
+
+  /* substituir localhost pela nova rota do backend  --> config: BudConfig
+  axios.create({
+    baseURL:
+    config.publicRuntimeConfig.api.newTaskManagement ??
+    `${config.publicRuntimeConfig.api.restBase}/new-task-management`
+  })*/
