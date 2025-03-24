fefe2dbe0b360fc476c9a396f1872f3f59c6ec7a
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2025-03-24 13:56:54 -0300

    remove refetch from invalidate queries

--- src/components/TaskManagement/hooks/new-task/use-update-task.ts
+++ src/components/TaskManagement/hooks/new-task/use-update-task.ts
@@ -1,15 +1,10 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation } from '@tanstack/react-query'
 import { useContext } from 'react'
 
 import { ServicesContext } from 'src/components/Base/ServicesProvider/services-provider'
 import { Task } from 'src/services/new-task-management/new-task-management.service'
-
-const MODULE = 'taskManager'
-const ACTION = 'getAllTasks'
-
 export function useUpdateTask() {
   const { servicesPromise } = useContext(ServicesContext)
-  const queryClient = useQueryClient()
 
   const updateTaskMutate = useMutation({
     mutationFn: async ({
@@ -24,9 +19,6 @@ export function useUpdateTask() {
       const { newTaskManagement } = await servicesPromise
       const response = await newTaskManagement.updateTask(teamId, taskId, data)
       return response
-    },
-    onSuccess: (_data, variables) => {
-      queryClient.invalidateQueries({ queryKey: [`${MODULE}:${ACTION}:${variables.teamId}`] })
     },
   })
   return updateTaskMutate
