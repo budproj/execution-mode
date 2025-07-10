90105af94b36931c875a6a8347c68205c5df589f
Kevin <kevin.c.souza@gmail.com>
2025-07-10 13:26:35 -0300

    debug answer drawer button

--- src/components/Routine/RetrospectiveTab/Answers/use-logic.ts
+++ src/components/Routine/RetrospectiveTab/Answers/use-logic.ts
@@ -121,14 +121,21 @@ export function useLogic({
 
   useEffect(() => {
     if (loadingAnswers) return
+    console.log(loadingAnswers)
     if (!dataAnswers) return
+    console.log(dataAnswers)
     if (!teamUsers) return
+    console.log(teamUsers)
 
     const isUserFromTeam = user?.id && teamUsers.some(({ id }) => id === user.id)
+    console.log(isUserFromTeam)
     const isActiveRoutine = isBefore(new Date(), before)
+    console.log(isActiveRoutine)
     const haveUserAnswered = dataAnswers?.find(
       (answer) => answer.userId === userID && answer.timestamp,
     )
+    console.log(haveUserAnswered)
+    console.log(dataAnswers)
     setShowAnswerNowButton(
       Boolean(isUserFromTeam && isActiveRoutine && !haveUserAnswered && dataAnswers?.length > 0),
     )
